43fa5769180127ed248416276ea1fbb14550f7f7
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-04-19 17:01:55 -0700

    Add debounce

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -454,3 +454,23 @@ export function closest(needle, haystack) {
     }
   });
 }
+
+/**
+ * Returns a function, that, as long as it continues to be invoked, will not
+ * be triggered. The function will be called after it stops being called for
+ * wait` milliseconds.
+*/
+export function debounce(func, wait, immediate) {
+  let timeout;
+  return function() {
+    const context = this, args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
