d03b1c5c5765ab81186901fd9af9a2949354b756
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-04-19 17:01:38 -0700

    Simplify component

--- src/components/tools/Settings/SelectThreshold.svelte
+++ src/components/tools/Settings/SelectThreshold.svelte
@@ -1,86 +1,71 @@
 <script>
   import { onMount, createEventDispatcher } from 'svelte';
   import {
-    TileGroup,
-    RadioTile,
+    RadioButtonGroup,
+    RadioButton,
+    RadioButtonSkeleton,
     NumberInput,
-    NumberInputSkeleton,
-    Button,
   } from 'carbon-components-svelte';
+  import { debounce } from '../../../helpers/utilities';
 
-  export let label = '';
-  export let defaultValue;
-  export let customValue;
-  export let useCustom = false;
-  export let units = '°F';
+  export let selectedId;
+  export let items;
+
+  console.log('from component', selectedId, items);
 
   const dispatch = createEventDispatcher();
-  
-  let openSet = false;
+  let selected = items.find(d => d.id === selectedId);;
   let ready = false;
+  let value = 100;
 
-  let userInput = 0;
-
-  let selected = 'default';
-  if (useCustom) {
-    selected = 'custom';
+  function change(e) {
+    if (!e.detail) return;
+    selected = items.find(d => d.id === e.detail);
+    if (selected.id === 'default') {
+      dispatch('change', selected);
+    }
   }
 
-  $: if (selected === 'default') {
-    useCustom = false;
-    dispatch('change', { type: 'default', value: defaultValue });
-  }
-
-  function updateCustomThreshold() {
-    customValue = userInput; // set threshold to user input
-    //openSet = false; // close modal
-    useCustom = true;
-    dispatch('change', { type: 'custom', value: customValue });
-  }
-
+  const setCustom = debounce((e) => {
+    console.log('on change', e.detail);
+    selected.value = value;
+    dispatch('change', selected);
+  }, 1000);
+  
   onMount(() => {
     ready = true;
     dispatch('ready');
   });
 </script>
 
-<style>
-  :global(.sm.bx--tile-group .bx--tile) {
-    min-height: 2rem;
-    padding: 0.5rem;      
-  }
-
-  :global(.sm.bx--tile-group .bx--tile__checkmark) {
-    top: 0.5rem;
-  }
-
-  :global(.sm.bx--tile-group .bx--tile-content) {
-    font-size: 0.8rem;
-  }
-
-  :global(.threshold .bx--number input[type='number']) {
-    min-width: 5rem;
-  }
-</style>
-
 {#if ready}
-  <TileGroup legend={label} bind:selected class="sm">
-    <RadioTile value="default" checked>
-      <span class="d-block">98th Percentile</span>
-      <span style="font-size: 0.75rem;font-weight:500;">{defaultValue} {units}</span>
-    </RadioTile>
-    <RadioTile value="custom">
-      <span class="d-block">Custom</span>
-      {#if selected === 'custom'}
-        <div class="d-flex align-items-center mt-2">
-          <NumberInput class="threshold" bind:value={userInput} />
-          &nbsp;{units}&nbsp;
-          <Button style="width:3rem;padding-right:12px;" kind="secondary" size="small" on:click={updateCustomThreshold}>Set</Button>        
-        </div>
-      {/if}
-    </RadioTile>
-  </TileGroup>
+  <RadioButtonGroup
+    orientation="vertical"
+    selected={selectedId}
+    on:change={change}>
+    {#each items as opt}
+      <RadioButton labelText={opt.label} value={opt.id}/>
+    {/each}
+  </RadioButtonGroup>
+  {#if selected.id === 'default'}
+    <div style="display:flex;align-items:start;">
+      <NumberInput
+        disabled={true}
+        style="padding-right:0;"
+        value={selected.value} />
+    </div>
+  {/if}
+  {#if selected.id === 'custom'}
+    <div style="display:flex;align-items:start;">
+      <NumberInput
+        style="padding-right:0;"
+        on:change={setCustom}
+        bind:value />
+    </div>
+  {/if}
 {:else}
-  <NumberInputSkeleton />
+  <RadioButtonGroup orientation="vertical">
+    <RadioButtonSkeleton />
+    <RadioButtonSkeleton />
+  </RadioButtonGroup>
 {/if}
-
