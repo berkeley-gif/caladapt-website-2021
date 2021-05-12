60fe1a63cfff0fae0564a8a791b71f73b61b9946
Shruti Mukhtyar <Shruti Mukhtyar>
2021-05-12 08:54:47 -0700

    Add stats to show Avg and Range

---
+++ src/components/tools/Stats/RangeAvg.svelte
@@ -0,0 +1,190 @@
+<script>
+  import { onMount, createEventDispatcher } from 'svelte';
+  import { extent, mean, merge } from 'd3-array';
+  import {
+    Button,
+    SkeletonText,
+    Tooltip,
+  } from 'carbon-components-svelte';
+  import Settings from '.../../../static/img/icons/gear.svg';
+  import ChangeTimePeriod from './ChangeTimePeriod2.svelte';
+
+  export let title = 'Observed Data';
+  export let subtitle = 'Baseline (1961-1990)';
+  export let units = 'inches';
+  export let note = '';
+  export let data;
+  export let showRange = false;
+  export let selectedRange = [1961, 1990];
+  export let isHistorical = true;
+
+  export let format = (d) => d;
+
+  const dispatch = createEventDispatcher();
+  let ready = false;
+  let showSettings = false;
+  let stats = null;
+
+  function subsetByYearRange(range) {
+    return function(d) {
+      return (d.date >= new Date(range[0], 0, 1) && d.date <= new Date(range[1], 11, 31));
+    }
+  }
+
+  function updateStats(e) {
+    const {period, range} = e.detail;
+    subtitle = `${period} (${range[0]}-${range[1]})`;
+    showSettings = false;
+    stats = calculateStats(data, range);
+  }
+
+  function calculateStats(_data, range) {
+    const yearCount = range[1] - range[0] + 1;
+    const content = [];
+    if (_data.length === 0) {
+      content.push({ label: `${yearCount} YEAR AVG`, value: '–' });
+      if (showRange) {
+        content.push({ label: `${yearCount} YEAR RANGE`, value: '–' });
+      }
+      return content;
+    }
+    const values = merge(_data.map(series => series.values));
+    const filteredData = values.filter(subsetByYearRange(range));
+    const avg = mean(filteredData, d => d.value);
+    content.push({ label: `${yearCount} YEAR AVG`, value: format(avg) });
+    if (showRange) {
+      const minmax = extent(filteredData, d => d.value);
+      content.push({
+        label: `${yearCount} YEAR RANGE`,
+        value: `${format(minmax[0])}–${format(minmax[1])}`
+      });
+    }
+    return content;
+  }
+
+  $: if (data) {
+    stats = calculateStats(data, selectedRange);
+  } else {
+    stats = null;
+  }
+
+  onMount(() => {
+    ready = true;
+    dispatch('ready');
+  });
+</script>
+
+<style lang="scss">
+  .stat-data {
+    display: flex;
+    justify-content: space-around;
+    margin: 0.75rem 0;
+  }
+
+  .stat-title {
+    font-size: 1rem;
+    font-weight: 600;
+  }
+
+  .stat-subtitle {
+    font-size: 0.85rem;
+    text-transform: uppercase;
+    color: #04797c;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+  }
+
+  .stat-data-label {
+    font-size: 0.75rem;
+    color: #51585e;
+  }
+
+  .stat-data-value {
+    font-size: 1.5rem;
+    line-height: 1.5;
+  }
+
+  .stat-data-units {
+    font-size: 0.8rem;
+  }
+
+  .stat-note {
+    margin-top: 1rem;
+    font-style: italic;
+    font-size: 0.75rem;
+    color: #51585e;
+    display: flex;
+    align-items: center;
+  }
+
+  #stat-tooltip {
+    font-style: normal;
+  }
+
+  :global(.settings.bx--btn--sm) {
+    padding: calc(0.375rem - 3px);
+  }
+
+  :global(.settings #icon) {
+    height: 1rem;
+    width: 1rem;
+  }
+</style>
+
+{#if ready && stats}
+  <div class="stat">
+    <!-- title -->
+    <div class="stat-header">
+      <div class="stat-title">{title}</div>
+      <div class="stat-subtitle">
+        {subtitle}
+        <span>
+          <Button
+            class="settings"
+            tooltipPosition="bottom"
+            tooltipAlignment="center"
+            iconDescription="Change Time Period"
+            size="small"
+            kind="ghost"
+            on:click={() => showSettings=true}>
+            { @html Settings }
+          </Button>
+        </span>
+      </div>      
+    </div>
+    <!-- values -->
+    <div class="stat-data">
+      {#each stats as item}
+        <div>
+          <div class="stat-data-label">{item.label}</div>
+          <div class="stat-data-value">{item.value}</div>
+          <div class="stat-data-units">{units}</div>
+        </div>
+      {/each}
+    </div>
+    <!-- note -->
+    {#if note}
+      <div class="stat-note">
+        {note}.
+        <Tooltip tooltipBodyId="stat-tooltip">
+          <div id="stat-tooltip">
+            <p>The range and average are calculated using data values from all selected models for the selected time period.</p>
+            <p>List of selected models can be changed in the Settings Panel under Models. Select preset or custom time periods in the Stats Panel.</p>
+          </div>
+        </Tooltip>
+      </div>
+    {/if}
+  </div>
+{:else}
+  <div class="stat">
+    <SkeletonText heading />
+    <SkeletonText paragraph lines={4} />
+  </div>
+{/if}
+
+<ChangeTimePeriod
+  open={showSettings}
+  {isHistorical}
+  on:change={updateStats}
+  on:cancel={() => showSettings = false} />
--- src/components/tools/Stats/index.js
+++ src/components/tools/Stats/index.js
@@ -1,2 +1,3 @@
 export { default as MinMaxAvg } from './MinMaxAvg.svelte';
+export { default as RangeAvg } from './RangeAvg.svelte';
 export { default as MonthsCount } from './MonthsCount.svelte';
