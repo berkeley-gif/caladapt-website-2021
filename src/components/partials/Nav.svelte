c5c685c282dd13a7f72c50d828c437f9bf06f731
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-05-19 16:55:01 -0700

    Close nav on click

--- src/components/partials/Nav.svelte
+++ src/components/partials/Nav.svelte
@@ -19,7 +19,11 @@
     class="bx--header__action bx--header__menu-trigger bx--header__menu-toggle">
       <svg data-carbon-icon="Menu20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="20" height="20" preserveAspectRatio="xMidYMid meet" role="img" focusable="false"><path d="M2 14.8H18V16H2zM2 11.2H18V12.399999999999999H2zM2 7.6H18V8.799999999999999H2zM2 4H18V5.2H2z"></path><title>Open Menu</title></svg>
   </button>
-  <nav aria-label="Cal-Adapt" class="bx--header__nav" class:expanded={open}>
+  <nav
+    aria-label="Cal-Adapt"
+    class="bx--header__nav"
+    class:expanded={open}
+    on:click={() => open = !open}>
     <ul aria-label="Cal-Adapt" class="bx--header__menu-bar" role="menubar">
       <li>
         <a href="/tools/" class="bx--header__menu-item" role="menuitem" tabindex="0" aria-current="{segment === 'tools' ? 'page' : undefined}">
