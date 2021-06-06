bc8cf91ea90e3982e01ccc10e04a20076500290c
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-06-05 17:22:50 -0700

    Try different setting for evaluating source map to speed up dev

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
         'process.env.NODE_ENV': JSON.stringify(mode)
       }),
     ].filter(Boolean),
-    devtool: dev && 'inline-cheap-module-source-map'
+    devtool: dev && 'eval-cheap-source-map'
   },
 
   server: {
