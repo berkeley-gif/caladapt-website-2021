13c8ebe4d631a4a77b890013010dad101f470bfa
Shruti Mukhtyar <Shruti Mukhtyar>
2021-06-22 07:33:49 -0700

    Add Return level charts

--- src/components/tools/Charts/index.js
+++ src/components/tools/Charts/index.js
@@ -1,3 +1,5 @@
 export { default as LineAreaChart } from './LineArea/Chart.svelte';
 export { default as ScatterChart } from './Scatter/Chart.svelte';
 export { default as HeatmapChart } from './Heatmap/Chart.svelte';
+export { default as ReturnLevelBoxChart } from './ReturnLevelBox/Chart.svelte';
+export { default as ReturnLevelCurveChart } from './ReturnLevelCurve/Chart.svelte';
