a9854d12138349d769e11428665f6917c5f5faca
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-07-04 19:46:20 -0700

    Update placeholder

--- src/components/tools/Settings/SelectDayOfYear.svelte
+++ src/components/tools/Settings/SelectDayOfYear.svelte
@@ -1,35 +1,39 @@
 <script>
-  import { createEventDispatcher } from 'svelte';
+  import { timeFormat } from 'd3-time-format';
+  import { createEventDispatcher, onMount } from 'svelte';
   import {
-    DatePicker,
     DatePickerInput,
   } from 'carbon-components-svelte';
 
-  export let value = '7/04';
+  export let value;
 
+  $: placeholder = timeFormat('%m/%d')(value);
+
+  let DatePicker;
   const dispatch = createEventDispatcher();
 
   function change(e) {
     dispatch('change', e.detail.selectedDates[0]);
   }
+
+  // DatePicker ability to append to HTML body throws a document node defined
+  // error everytime component is rebuild
+  // https://github.com/IBM/carbon-components-svelte/issues/522
+  onMount(async() => {
+    const module = await import('carbon-components-svelte/src/DatePicker/DatePicker.svelte');
+    DatePicker = module.default;
+  });
 </script>
 
-<style>
-  .flatpickr-calendar .numInputWrapper {
-    display: none;
-  }
-</style>
-
-
 <div class="doy-select">
-  <DatePicker
-    {value}
+  <svelte:component
+    this={DatePicker}
     datePickerType="single"
     dateFormat="m/d"
     on:change={change}>
-    <DatePickerInput
-      hideLabel
-      placeholder={value}
-      pattern="\\d{1,2}\\/\\d{1,2}" />
-  </DatePicker>  
+      <DatePickerInput
+        hideLabel
+        {placeholder}
+        pattern="\\d{1,2}\\/\\d{1,2}" />
+  </svelte:component>
 </div>
