8149bdea7f5cbdce8762aaefab538242c01e58d6
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-07-13 15:35:14 -0700

    Remove inRange

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -1,5 +1,4 @@
-import { timeParse, utcParse } from 'd3-time-format';
-import { zip, transpose } from 'd3-array';
+import { utcParse } from 'd3-time-format';
 import { timeDay } from 'd3-time';
 import config from './api-config';
 import shp from 'shpjs';
