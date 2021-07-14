573a6dba3bc3fa30e5d4d51868aabf93a6e02297
Shruti Mukhtyar <Shruti Mukhtyar>
2021-07-14 08:59:22 -0700

    Switch forecast display to line chart

--- src/components/tools/Charts/Forecast/Chart.svelte
+++ src/components/tools/Charts/Forecast/Chart.svelte
@@ -1,57 +1,120 @@
 <script>
-  import { SkeletonPlaceholder } from 'carbon-components-svelte';
+  import { SkeletonText, SkeletonPlaceholder } from 'carbon-components-svelte';
+  import { LayerCake, Svg, Html } from 'layercake';
+  import { scaleTime } from 'd3-scale';
+  import { timeFormat } from 'd3-time-format';
+  import { min, max } from 'd3-array';
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
+
+  import Line from './Line.svelte';
+  import AxisX from './AxisX.svelte';
+  import AxisY from './AxisY.svelte';
+  import Tooltip from '../Shared/Tooltip.svelte';
 
   export let data;
   export let height = '350px';
-  export let units;
+  export let tooltip = {
+    title: '',
+  };
+  export let yAxis = {
+    key: 'value',
+    label: 'YAxis Label',
+    baseValue: null,
+    tickFormat: (d) => d,
+    units: '',
+  }
+  export let xAxis = {
+    key: 'date',
+    label: 'XAxis Label',
+    baseValue: null,
+    tickFormat: timeFormat('%Y'),
+    units: '',
+  }
 
   let chartContainer;
+  let xmin;
+  let xmax;
+  let ymin;
+  let ymax;
+  
+  $: if (data) {
+    // Set X Domain
+    xmin = min(data, d => d.date);
+    xmax = max(data, d => d.date);
+    if (xAxis.baseValue === 0) {
+      xmin = xAxis.baseValue;
+    }
 
+    // Set Y Domain
+    ymin = min(data, d => d.value);
+    ymax = max(data, d => d.value);
+    if (yAxis.baseValue === 0) {
+      ymin = yAxis.baseValue;
+    }
+  }
+
+  function getTooltipLabel(d) {
+    const item = data.find(series => series.key === d);
+    if (item) {
+      return item.label;
+    } else {
+      d;
+    }
+  }
+
+  function getTooltipValue(d) {
+    return `${yAxis.tickFormat(d)}`;
+  }
+
+  function getTooltipTitle(d) {
+    return `${d} ${tooltip.title}`;
+  }
 </script>
-
-<style>
-  .items {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .item {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-    flex: 0 0 32%;
-    margin: 0.25rem;
-    border: 1px solid #bcc2c7;
-  }
-
-  .item .day {
-    font-size: 1.1rem;
-    padding: 0.5rem;
-    text-transform: uppercase;
-  }
-
-  .item .value {
-    font-size: 1.5rem;
-    font-weight: bold;
-    padding: 0.5rem;
-  }
-</style>
 
 {#if data}
   <div style={`height:${height}`} bind:this={chartContainer}>
-    <div class="items">
-      {#each data as opt}
-        <div class="item">
-          <span class="day">{opt.day}</span>
-          <span class="value">{opt.value} {units}</span>
-          <span>{opt.label}</span>
-        </div>
-      {/each}
-    </div>
-  </div>
+    <LayerCake
+      padding={{ top: 40, right: 40, bottom: 40, left: 40 }}
+      x={xAxis.key}
+      y={yAxis.key}
+      xScale ={scaleTime()}
+      xDomain={[ xmin, xmax ]}
+      yDomain={[ ymin, ymax ]}
+      data={data}>
+        <Svg>
+          <AxisX
+            formatTick={xAxis.tickFormat}
+            baseline={true}
+            gridlines={false}
+            snapTicks={false}
+            ticks={data.map(d => d.date)}
+          />
+          <AxisY
+            label={yAxis.label}
+            gridlines={true}
+          />
+          <g class="line-group">
+            <Line/>
+          </g>
+        </Svg>
+        <Html>
+<!--           <Tooltip
+            dataset={dataByDate}
+            title={getTooltipTitle}
+            label={getTooltipLabel}
+            value={getTooltipValue}
+          /> -->
+        </Html>
+      </LayerCake>
+  </div> 
 {:else}
+  <div class="chart-legend">
+    <SkeletonText />
+    <SkeletonText />
+  </div>
   <div style={`height:${height}`}>
     <SkeletonPlaceholder style="height:100%;width:100%;" />
   </div>
 {/if}
+
---
+++ src/components/tools/Charts/Forecast/Line.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { getContext } from 'svelte';
+
+  const { xGet, yGet, data } = getContext('LayerCake');
+
+  $: path = values => {
+    return 'M' + values
+      .map(d => {
+        return $xGet(d) + ',' + $yGet(d);
+      })
+      .join('L');
+  };
+</script>
+
+
+<path
+  class={`path-line forecast`}
+  d='{path($data)}'
+  stroke="#9fa6ac"
+  fill="none"
+  stroke-width="1px">
+</path>
+<g>
+  {#each $data as d}
+    <text
+      x={$xGet(d)}
+      y={$yGet(d)}
+      dx="-10"
+      dy="-10"
+      style="font-size:1.2rem;text-shadow: #FFF 1px 0 8px;"
+    >
+      {d.value} °F
+    </text>
+    <circle
+      cx={$xGet(d)}
+      cy={$yGet(d)}
+      r={4}
+      fill="#002448"
+      fill-opacity={0.8}
+      stroke="blue"
+      stroke-width={1}
+    />
+  {/each}
+</g>
+
