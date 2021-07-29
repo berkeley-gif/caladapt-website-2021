0bdf2b180c971e3d80c21ef3bd9d00004252700b
Chris Henrick <chrishenrick@gmail.com>
2021-07-29 12:00:45 -0700

    added descriptions for get-started topics

--- src/routes/help/get-started/_getting-started-topics.js
+++ src/routes/help/get-started/_getting-started-topics.js
@@ -1,21 +1,27 @@
-export const getting_started_topics = [{
-  slug: 'climate-change-and-climate-data-in-california',
-  title: 'Climate change and climate data in California',
-  text: 'lorem ipsum...',
-}, {
-  slug: 'climate-data-and-other-data-on-caladapt',
-  title: 'Climate data and other data on Cal Adapt',
-  text: 'lorem impsum...',
-}, {
-  slug: 'about-climate-projections-and-models',
-  title: 'About climate projections and models',
-  text: 'lorem impsum...',
-}, {
-  slug: 'accessing-data-on-cal-adapt',
-  title: 'Accessing data on Cal-Adapt',
-  text: 'lorem impsum...',
-}, {
-  slug: 'best-practices-for-using-climate-projections',
-  title: 'Best practices for using climate projections',
-  text: 'lorem impsum...',
-}]
+export const getting_started_topics = [
+  {
+    slug: "climate-change-and-climate-data-in-california",
+    title: "Climate change and climate data in California",
+    text: "Why climate data is important for adaptation planning and how to use it.",
+  },
+  {
+    slug: "climate-data-and-other-data-on-caladapt",
+    title: "Climate data and other data on Cal Adapt",
+    text: "Information about the types of data available through Cal Adapt.",
+  },
+  {
+    slug: "about-climate-projections-and-models",
+    title: "About climate projections and models",
+    text: "How climate models are generated, validated, and account for greenhouse gas emission scenarios.",
+  },
+  {
+    slug: "accessing-data-on-cal-adapt",
+    title: "Accessing data on Cal-Adapt",
+    text: "Three ways of exploring and downloading the data available on Cal Adapt.",
+  },
+  {
+    slug: "best-practices-for-using-climate-projections",
+    title: "Best practices for using climate projections",
+    text: "Understanding uncertainty, natural variability, and how to use climate projections in your work.",
+  },
+];
