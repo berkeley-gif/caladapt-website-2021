c39941f23791cb74ea62598228ee0b0433d26f9c
Chris Henrick <chrishenrick@gmail.com>
2021-07-30 12:01:52 -0700

    updated SupportFooter: add optional top border

--- src/routes/help/_SupportFooter.svelte
+++ src/routes/help/_SupportFooter.svelte
@@ -2,11 +2,20 @@
   p.feedback {
     text-align: left;
     float: none;
-    margin: 0;
+    margin: 0.5rem 1rem;
+  }
+
+  .rule {
+    height: 1px;
+    background-color: var(--gray-30);
+    margin: 0 1rem;
   }
 </style>
 
 <div class="footer">
+  {#if borderTop}
+    <div class="rule"></div>
+  {/if}
   <p class="feedback">
     Email <a href="mailto:{address}">{address}</a> with your feedback on this topic.
   </p>
@@ -14,4 +23,5 @@
 
 <script>
   export let address = "support@cal-adapt.org";
+  export let borderTop = true;
 </script>
