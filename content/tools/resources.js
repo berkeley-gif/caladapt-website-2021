4dd45a7ee173d28e00c5aa7bf1a12cba99289e0d
Shruti Mukhtyar <Shruti Mukhtyar>
2021-08-15 12:27:31 -0700

    Track list of external resources

---
+++ content/tools/resources.js
@@ -0,0 +1,23 @@
+const resources = [
+  {
+    title: "California's Adaptation Clearinghouse",
+    link: "https://resilientca.org",
+    image: "logos/EJ_JRC_ccc_simulation-7094.jpg",
+    desc: `The Adaptation Clearinghouse is the State of California’s consolidated searchable 
+    database of resources for local, regional and statewide climate adaptation planning 
+    and decision-making.`,
+    category: 'external',
+  },
+  {
+    title: "California's 4th Climate Change Assessment",
+    link: "https://www.climateassessment.ca.gov/",
+    image: "logos/ccc4a.jpg",
+    desc: `California’s Fourth Climate Change Assessment provides information to 
+    build resilience to climate impacts, including temperature, wildfire, water, 
+    sea level rise, and governance. The assessment includes reports for nine regions 
+    of the state to support action at local and regional scales.`,
+    category: 'external',
+  },
+];
+
+export default resources;
