809854589dcb44bbca8ddf38ff4b4090d25db813
Shruti Mukhtyar <Shruti Mukhtyar>
2021-08-15 12:35:56 -0700

    Change default overlay style and map style

--- src/components/tools/Location/StaticMap.svelte
+++ src/components/tools/Location/StaticMap.svelte
@@ -1,24 +1,29 @@
 <script>
   import simplifyGeom from "@turf/simplify";
+
   // Helpers
   import { mapboxgl } from "./../../../helpers/mapbox";
 
   // Props
-  //-------
   export let width = 150;
   export let height = 150;
   export let location = null;
-  export let zoom = 5;
-  export let style = "cal-adapt/cjivy3e8o6d9y2rnnhsqo0sj0";
+  //export let style = "cal-adapt/cjivy3e8o6d9y2rnnhsqo0sj0";
+  export let style = "mapbox/streets-v11";
+  export let padding = 50;
 
   $: geometry = location.geometry;
   $: if (geometry.type === "Polygon" || geometry.type === "MultiPolygon") {
     geometry = simplifyGeom(geometry, { tolerance: 0.01, mutate: true });
   }
-  $: overlay = encodeURIComponent(JSON.stringify(geometry));
-  $: lon = location.center[0];
-  $: lat = location.center[1];
-  $: src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${lon},${lat},${zoom}/${width}x${height}?access_token=${mapboxgl.accessToken}`;
+  $: overlay = encodeURIComponent(JSON.stringify({
+      type: "Feature",
+      properties: {
+        "fill-opacity": 0,
+      },
+      geometry,
+    }));
+  $: src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/auto/${width}x${height}@2x?padding=${padding}&access_token=${mapboxgl.accessToken}`;
 
   let container;
 </script>
