274ee3ef833e90a9880e7863b0da4f1f304ffeb6
Shruti Mukhtyar <Shruti Mukhtyar>
2021-08-17 00:01:37 -0700

    Move header styles to scss

--- src/routes/tools/extreme-weather/Header.svelte
+++ src/routes/tools/extreme-weather/Header.svelte
@@ -11,52 +11,14 @@
   }
 </script>
 
-<style type="scss">
-  .header {
-    padding: 2rem;
-    background: rgba(218, 222, 225, 0.25);
-  }
-
-  .secondary-nav {
-    width: 100%;
-    background: rgba(250, 251, 251, 1);
-    top: 0;
-    border-bottom: 1px solid #dadee1;
-    z-index: 3;
-    background: var(--gray-30);
-  }
-
-  .secondary-nav ul {
-    display: flex;
-    flex-wrap: wrap;
-    list-style-type: none;
-    text-transform: uppercase;
-    margin: 0 2rem;
-  }
-
-  .secondary-nav ul li {
-    margin-right: 1rem;
-    cursor: pointer;
-    padding: 0.5rem;
-    color: #000;
-  }
-
-  .secondary-nav ul li:hover {
-    background: rgba(154, 201, 202, 0.5);
-  }
-
-  .secondary-nav ul li.active {
-    font-weight: 600;
-  }
-</style>
-
 <div class="header">
   <h1>Extreme Weather</h1>
   <p class="lead">
-    Explore extreme temperatures for past weather and present day at 39 weather
-    stations across California, utilizing a quality-controlled data base for
-    hourly weather observations for 1973-2019 produced for use by the energy
-    sector.
+    Explore extreme temperatures for past weather and present day at 38 weather
+    stations across California. Each of the 38 stations has an observation
+    period of greater than 30 years (1973 to present) from the HadISD global
+    record and was identified as having high quality data for temperature. This
+    dataset was produced for use by the energy sector.
   </p>
 </div>
 <div class="secondary-nav is-sticky">
