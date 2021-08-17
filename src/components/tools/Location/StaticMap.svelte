36e0b1aff6328a7e0aca8458017f65ba75cc1f42
Shruti Mukhtyar <Shruti Mukhtyar>
2021-08-17 00:03:24 -0700

    Reduce simplification

--- src/components/tools/Location/StaticMap.svelte
+++ src/components/tools/Location/StaticMap.svelte
@@ -8,7 +8,6 @@
   export let width = 150;
   export let height = 150;
   export let location = null;
-  //export let style = "cal-adapt/cjivy3e8o6d9y2rnnhsqo0sj0";
   export let style = "mapbox/streets-v11";
   export let padding = 50;
   export let zoom = 8;
@@ -21,36 +20,28 @@
   $: if (location) {
     geometry = location.geometry;
     if (geometry.type === "Point") {
+      // Use zoom for point feature to prevent returned image from
+      // being zoomed all the way in
       bounds = `${geometry.coordinates[0]},${geometry.coordinates[1]},${zoom}`;
       overlay = encodeURIComponent(JSON.stringify(geometry));
-      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}@2x?&access_token=${mapboxgl.accessToken}`;
+      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}?&access_token=${mapboxgl.accessToken}`;
     } else {
-      geometry = simplifyGeom(geometry, { tolerance: 0.01, mutate: true });
+      // Add style for non point geometry
+      geometry = simplifyGeom(geometry, { tolerance: 0.1, mutate: true });
       bounds = "auto";
       overlay = encodeURIComponent(
         JSON.stringify({
           type: "Feature",
           properties: {
             "fill-opacity": 0,
+            "stroke-width": 2,
           },
           geometry,
         })
       );
-      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}@2x?padding=${padding}&access_token=${mapboxgl.accessToken}`;
+      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}?padding=${padding}&access_token=${mapboxgl.accessToken}`;
     }
   }
-
-  // $: geometry = location.geometry;
-  // $: if (geometry.type === "Polygon" || geometry.type === "MultiPolygon") {
-  //   geometry = simplifyGeom(geometry, { tolerance: 0.01, mutate: true });
-  //   bounds = "auto";
-  // } else {
-  //   bounds = `${geometry.coordinates[0]},${geometry.coordinates[1]},${zoom}`;
-  //   console.log('bounds', bounds);
-  // }
-  // $:
-
-  // $: src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}@2x?padding=${padding}&access_token=${mapboxgl.accessToken}`;
 
   let container;
 </script>
