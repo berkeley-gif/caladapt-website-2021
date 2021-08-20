e962719de662c8968ada75503a3a61bba42c5780
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-08-19 17:12:39 -0700

    Fix static map settings. Add error handling

--- src/components/tools/Location/StaticMap.svelte
+++ src/components/tools/Location/StaticMap.svelte
@@ -1,5 +1,5 @@
 <script>
-  import simplifyGeom from "@turf/simplify";
+  import simplify from "@turf/simplify";
 
   // Helpers
   import { mapboxgl } from "./../../../helpers/mapbox";
@@ -11,41 +11,91 @@
   export let style = "mapbox/streets-v11";
   export let padding = 50;
   export let zoom = 8;
+  export let alt = "location map";
 
-  let geometry;
-  let bounds;
-  let overlay;
+  let img;
   let src;
+  let bounds;
+  let geojson;
+  let overlay;
+  let loading;
+  let error;
 
-  $: if (location) {
-    geometry = location.geometry;
-    if (geometry.type === "Point") {
-      // Use zoom for point feature to prevent returned image from
-      // being zoomed all the way in
-      bounds = `${geometry.coordinates[0]},${geometry.coordinates[1]},${zoom}`;
-      overlay = encodeURIComponent(JSON.stringify(geometry));
-      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}?&access_token=${mapboxgl.accessToken}`;
-    } else {
-      // Add style for non point geometry
-      geometry = simplifyGeom(geometry, { tolerance: 0.1, mutate: true });
-      bounds = "auto";
-      overlay = encodeURIComponent(
-        JSON.stringify({
-          type: "Feature",
-          properties: {
-            "fill-opacity": 0,
-            "stroke-width": 2,
-          },
-          geometry,
-        })
-      );
-      src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}?padding=${padding}&access_token=${mapboxgl.accessToken}`;
-    }
+  function load() {
+    loading = true;
+    error = false;
+    img.src = src;
+    img.onload = () => {
+      loading = false;
+    };
+    img.onerror = () => {
+      error = true;
+    };
   }
 
-  let container;
+  $: if (location) {
+    if (location.geometry.type === "Point") {
+      geojson = location.geometry;
+      // Use zoom for point geometry to prevent returned image from
+      // being zoomed all the way in
+      bounds = `${geojson.coordinates[0]},${geojson.coordinates[1]},${zoom}`;
+      overlay = encodeURIComponent(JSON.stringify(geojson));
+    } else {
+      // Add style for non point geometry
+      geojson = {
+        type: "Feature",
+        properties: {
+          "fill-opacity": 0,
+          "stroke-width": 3,
+        },
+        geometry: location.geometry,
+      };
+      const simplifiedGeojson = simplify(geojson, { tolerance: 0.01 });
+      bounds = "auto";
+      overlay = encodeURIComponent(JSON.stringify(simplifiedGeojson));
+    }
+    src = `https://api.mapbox.com/styles/v1/${style}/static/geojson(${overlay})/${bounds}/${width}x${height}?padding=${padding}&access_token=${mapboxgl.accessToken}`;
+  }
+
+  $: if (img && src !== undefined) load();
 </script>
 
-<div class="static-map" bind:this="{container}">
-  <img src="{src}" alt="Location map" />
+<style>
+  div.static-map {
+    position: relative;
+  }
+  img {
+    width: 100%;
+  }
+  img.loading {
+    opacity: 0;
+  }
+  img:not(.loading) {
+    opacity: 1;
+    transition: opacity 250ms ease-out;
+  }
+  .hide {
+    display: none;
+  }
+</style>
+
+<div class="static-map">
+  {#if loading}
+    <span>Loading map of location...</span>
+  {/if}
+  {#if error}
+    <span>Unable to load map</span>
+  {/if}
+  <img
+    {...$$restProps}
+    bind:this="{img}"
+    class:loading
+    class:hide="{error}"
+    on:click
+    on:mouseover
+    on:mouseenter
+    on:mouseout
+    src="{src}"
+    alt="{alt}"
+  />
 </div>
