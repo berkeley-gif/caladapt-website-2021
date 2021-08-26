c8f55281ce4d868ad65d5195c3dd4a6e37122b4d
Chris Henrick <161748+clhenrick@users.noreply.github.com>
2021-08-26 15:40:55 -0700

    Data download page (#14)
    * added markup for top level data page
    
    * added helper css classes for vertical spacing
    
    * set default width of all p elements
    
    * adjust markup in data page:
    - rm section elements
    - use bleed css class
    
    * added helper css classes for vertical margins
    
    * fix use of @include; make all heading elements semi-bold
    
    * adjust grid layout for lg breakpoint
    
    * set max-width for all p.lead instances
    
    * style links as primary buttons

--- src/routes/data/index.svelte
+++ src/routes/data/index.svelte
@@ -1,12 +1,23 @@
 <script>
+  import { Button } from "carbon-components-svelte";
+  import { ArrowRight16 } from "carbon-icons-svelte";
 </script>
 
 <style>
   .banner {
-    background-image: url(/img/banners/data.svg);
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+      url(/img/banners/data.svg);
     background-position: center;
     background-size: cover;
-    min-height: 275px;
+    padding: var(--spacing-48) 0;
+  }
+
+  p:not(.lead) {
+    font-size: 1.125rem;
+  }
+
+  p.btn-container {
+    text-transform: uppercase;
   }
 </style>
 
@@ -15,19 +26,139 @@
 </svelte:head>
 
 <!-- Banner -->
-<section class="banner overlay">
+<div class="bleed banner overlay">
   <div class="bx--grid">
     <div class="bx--row">
-      <div class="bx--col">
-        <h1>Climate Data</h1>
+      <div class="bx--col-lg-2"></div>
+      <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+        <h1>Download Data</h1>
         <p class="lead">
-          We provide several ways to download data. <a
-            href="/help/faqs/how-can-i-download-data/">Learn more</a
-          >.
+          Discover and access climate data for California from the state's
+          scientific and research community hosted on Cal-Adapt in the following
+          ways.
         </p>
       </div>
+      <div class="bx--col-lg-5"></div>
     </div>
   </div>
-</section>
+</div>
 
-<section class="page-content"></section>
+<div class="bx--grid bx--grid--condensed">
+  <div class="bx--row margin--v-32">
+    <div class="bx--col-lg-2"></div>
+    <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+      <h2>Climate Tools</h2>
+      <p>
+        All Cal-Adapt climate tools have a “Download Chart” button that you may
+        use to download all data and graphics displayed on screen. Any applied
+        filters, selected date ranges, or other options and recalculations are
+        applied to the exported data. For downloading complete and original time
+        series data, use the Data Download Tool, Data Server, or API.
+      </p>
+      <p class="btn-container">
+        <Button icon="{ArrowRight16}" href="/tools/">Climate Tools</Button>
+      </p>
+    </div>
+    <div class="bx--col-lg-5"></div>
+  </div>
+
+  <div class="bx--row margin--v-32">
+    <div class="bx--col-lg-2"></div>
+    <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+      <h2>Data Download Tool</h2>
+      <p>
+        Search by climate indicator, publisher or dataset name. Follow a series
+        of steps to select a download format (GeoTIFF/CSV), spatial extent,
+        emissions scenarios and climate models. Certain datasets have the option
+        of downloading resampled data using different spatial and temporal
+        aggregations.
+      </p>
+      <p class="btn-container">
+        <Button icon="{ArrowRight16}" href="/data/download/"
+          >Data Download Tool</Button
+        >
+      </p>
+    </div>
+    <div class="bx--col-lg-5"></div>
+  </div>
+
+  <div class="bx--row margin--v-32">
+    <div class="bx--col-lg-2"></div>
+    <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+      <h2>Cal-Adapt Data Server</h2>
+      <p>
+        All climate data provided by California’s scientific and research
+        community is stored in their original formats on the Cal-Adapt Data
+        Server. Gridded data are commonly formatted as NetCDF files, while
+        station data is commonly formatted as CSV files. Some datasets (e.g. sea
+        level rise flood extents) are available as GeoTIFF files. Datasets on
+        the server are organized into directories by data provider/source.
+        Navigate the directory structure and select files you want to download.
+        Links to individual datasets are also included in the Data Download
+        Tool.
+      </p>
+      <p class="btn-container">
+        <Button
+          icon="{ArrowRight16}"
+          href="http://albers.cnr.berkeley.edu/data/"
+          >Cal-Adapt Data Server</Button
+        >
+      </p>
+    </div>
+    <div class="bx--col-lg-5"></div>
+  </div>
+
+  <div class="bx--row margin--v-32">
+    <div class="bx--col-lg-2"></div>
+    <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+      <h2>Cal-Adapt API</h2>
+      <p>
+        The Cal-Adapt REST API allows you to programmatically access climate
+        data on Cal-Adapt. If you need help getting started, explore code
+        samples developed by our developers for processing, downloading, and
+        visualizing Cal-Adapt data using the <a
+          href="https://berkeley-gif.github.io/caladapt-docs/cookbook.html"
+          >Python</a
+        >
+        and
+        <a href="https://observablehq.com/collection/@mukhtyar/cal-adapt"
+          >JavaScript</a
+        > programming languages.
+      </p>
+      <p class="btn-container">
+        <Button
+          icon="{ArrowRight16}"
+          href="https://berkeley-gif.github.io/caladapt-docs/"
+          >Cal-Adapt API documentation</Button
+        >
+      </p>
+    </div>
+    <div class="bx--col-lg-5"></div>
+  </div>
+
+  <div class="bx--row margin--v-32">
+    <div class="bx--col-lg-2"></div>
+    <div class="bx--col-lg-9 bx--col-md-8 bx--col-sm-4">
+      <h2>3rd Party Integrations</h2>
+      <p>
+        The following packages enable working with the Cal-Adapt API using the R
+        programming language and the ArcGIS Pro desktop software.
+      </p>
+      <p class="btn-container">
+        <Button
+          icon="{ArrowRight16}"
+          href="https://ucanr-igis.github.io/caladaptr/"
+          >Cal-Adapt R package</Button
+        >
+      </p>
+      <p class="btn-container">
+        <Button icon="{ArrowRight16}" href="/data/#"
+          >ArcGIS Pro Extension</Button
+        >
+      </p>
+    </div>
+  </div>
+  <div class="bx--col-lg-5"></div>
+</div>
+
+<div class="spacing--v-96"></div>
