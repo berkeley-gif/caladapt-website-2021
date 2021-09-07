e5612447fd60567591f31bf52b4bf497243d1d59
Chris Henrick <chrishenrick@gmail.com>
2021-09-07 10:08:01 -0700

    added new component: TweetButton

---
+++ src/components/social-media/TweetButton.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let text = "The latest from Cal-Adapt";
+  export let url = "https://cal-adapt.org";
+  export let hashtags = [];
+  export let via = "cal_adapt";
+</script>
+
+<div>
+  <a
+    class="twitter-share-button"
+    href="https://twitter.com/intent/tweet"
+    data-dnt="true"
+    data-text="{text}"
+    data-url="{url}"
+    data-hashtags="{Array.isArray(hashtags) && hashtags.length
+      ? hashtags.join(',')
+      : ''}"
+    data-via="{via}"
+  >
+    Tweet</a
+  >
+</div>
