7b5f6b78bc37461367b801aebb1f99cc398c4124
Shruti Mukhtyar <Shruti Mukhtyar>
2021-09-06 20:59:33 -0700

    Use Dashboard component

--- src/routes/tools/extreme-weather/ExploreData.svelte
+++ src/routes/tools/extreme-weather/ExploreData.svelte
@@ -1,9 +1,7 @@
 <script>
   import {
     Button,
-    Modal,
-    Accordion,
-    AccordionItem,
+    Loading,
     NumberInput,
     RadioButtonGroup,
     RadioButton,
@@ -11,6 +9,7 @@
     InlineLoading,
   } from "carbon-components-svelte";
   import { format } from "d3-format";
+  import { min, max, extent } from "d3-array";
   import { Download16, Share16, Location16 } from "carbon-icons-svelte";
 
   // Helpers
@@ -29,13 +28,14 @@
   } from "./_data";
 
   // Components
+  import { Dashboard } from "~/components/tools/Partials";
   import {
     SelectClimvar,
     SelectDayOfYear,
     ShowDefinition,
   } from "~/components/tools/Settings";
   import { StaticMap } from "~/components/tools/Location";
-  import { Histogram } from "~/components/tools/Charts";
+  import { Histogram, Observations } from "~/components/tools/Charts";
   import { notifier } from "~/components/notifications";
 
   // Store
@@ -47,7 +47,7 @@
     doyRange,
     thresholdStore,
     hadisdStore,
-    datasetStore,
+    //datasetStore,
     extremesStore,
     forecastDate,
     forecastStore,
@@ -61,7 +61,9 @@
   const { doyText } = doyStore;
   const { climvar } = climvarStore;
   const { location } = locationStore;
-  const { titles } = datasetStore;
+  //TODO: Use dataset info from API after data download tool is migrated
+  //const { titles } = datasetStore;
+  const titles = ["Hourly Observed Historical Data (Met Office Hadley Centre)"];
 
   let isLoading = true;
   let showDownload = false;
@@ -79,8 +81,6 @@
 
   let histogramData;
   let histogramAnnotation;
-  let recordLow;
-  let recordHigh;
 
   let threshBound;
   let threshInvalidText;
@@ -97,8 +97,10 @@
   let measured = null;
   let measuredLoading = false;
 
+  let xDomain;
+
   $: formatFn = format(`.${$climvar.decimals}f`);
-  $: dataSourceTitles = $titles;
+  $: dataSourceTitles = titles;
   $: $locationStore, resetObservations();
   $: $climvar, updateObservations();
 
@@ -108,14 +110,18 @@
       $baseline.percentiles,
       $extremesStore
     );
-    recordLow = $baseline.low;
-    recordHigh = $baseline.high;
     threshBound = filterThreshold($baseline.percentiles, $extremesStore);
     threshInvalidText = getThresholdText($extremesStore);
     thresholdStore.set(threshBound);
+    //xDomain = updateDataExtent([...$baseline.dataExtent, $thresholdStore]);
     isLoading = false;
   } else {
     isLoading = true;
+  }
+
+  function updateDataExtent(arr) {
+    const dataExtent = extent(arr);
+    return [dataExtent[0] - 2, dataExtent[1] + 2];
   }
 
   async function loadShare() {
@@ -137,12 +143,12 @@
       ["station", $location.title],
       ["variable", $climvar.label],
       ["units", $climvar.units.imperial],
-      ["record high", recordHigh],
-      ["record low", recordLow],
+      ["record high", `{$baseline.high.value} on {$baseline.high.date}`],
+      ["record low", `{$baseline.low.value} on {$baseline.low.date}`],
     ];
     csvData = $baseline.values;
-    printContainer = document.querySelector(".explore");
-    printSkipElements = ["explore-settings"];
+    printContainer = document.querySelector("#explore");
+    printSkipElements = ["settings", "chart-controls"];
     DownloadChart = (
       await import("~/components/tools/Partials/DownloadChart.svelte")
     ).default;
@@ -193,6 +199,7 @@
     if (!threshValid) return;
     thresholdStore.set(+e.detail);
     console.log("threshold change");
+    xDomain = updateDataExtent([...$baseline.dataExtent, +e.detail]);
     threshProbability = calcThresholdProbability({
       gevisf: $gevisf,
       threshold: +e.detail,
@@ -200,6 +207,7 @@
     threshExceedances = calcThresholdExceedances({
       values: histogramData,
       threshold: +e.detail,
+      extremes: $extremesStore,
     });
     threshCI = await getProbabilityCI(threshProbability.rp);
     threshCIStore.set(threshCI);
@@ -233,14 +241,11 @@
       forecastStore.set(data);
       forecast = filterForecast($climvar.id, data);
       forecastLoading = false;
-      dataSourceTitles = [...dataSourceTitles, "National Weather Service"];
       return data;
     } catch (error) {
       console.log("error fetching forecast");
       forecastLoading = false;
-      dataSourceTitles = dataSourceTitles.filter(
-        (d) => d !== "National Weather Service"
-      );
+      dataSourceTitles = dataSourceTitles.filter((d) => !d.includes("NWS"));
       showForecast = false;
       notifier.error(
         "Error",
@@ -253,9 +258,12 @@
   function toggleForecastDisplay() {
     showForecast = !showForecast;
     if (showForecast) {
+      dataSourceTitles = [...dataSourceTitles, "Near-Term Forecast (NWS)"];
       if (forecast === null) {
         getForecast();
       }
+    } else {
+      dataSourceTitles = dataSourceTitles.filter((d) => !d.includes("NWS"));
     }
   }
 
@@ -267,25 +275,18 @@
         startDate: $measuredDateRange.start,
         endDate: $measuredDateRange.end,
       });
-      console.log("data", data);
       measuredStore.set(data);
       measured = filterMeasured($climvar.id, data);
       measuredLoading = false;
-      dataSourceTitles = [
-        ...dataSourceTitles,
-        "National Centers for Environmental Information",
-      ];
       return data;
     } catch (error) {
       console.log("error fetching noaa data");
       measuredLoading = false;
       showMeasured = false;
-      dataSourceTitles = dataSourceTitles.filter(
-        (d) => d !== "National Centers for Environmental Information"
-      );
+      dataSourceTitles = dataSourceTitles.filter((d) => !d.includes("NCEI"));
       notifier.error(
         "Error",
-        "Unable to get recent observations from NOAA at this time. Try again in a few minutes.",
+        "Unable to get recent observations from NCEI at this time. Try again in a few minutes.",
         2000
       );
     }
@@ -294,80 +295,133 @@
   function toggleMeasuredDisplay() {
     showMeasured = !showMeasured;
     if (showMeasured) {
+      dataSourceTitles = [...dataSourceTitles, "GHCN-Daily (NCEI)"];
       if (measured === null) {
         getMeasured();
       }
+    } else {
+      dataSourceTitles = dataSourceTitles.filter((d) => !d.includes("NCEI"));
     }
   }
 </script>
 
-<style>
-  .block-title.threshold {
-    color: red;
+<style lang="scss">
+  .block {
+    background-color: var(--white);
+    box-shadow: var(--box-shadow);
+    height: 100%;
+    box-sizing: border-box;
+    padding: var(--spacing-16);
+  }
+
+  .annotate {
+    font-weight: 600;
+
+    &.threshold {
+      color: red;
+    }
+  }
+
+  .h4 {
+    font-weight: 400;
+  }
+
+  .title {
+    > * {
+      margin: var(--spacing-8) 0;
+    }
+
+    .h3 {
+      margin-top: 0;
+    }
+  }
+
+  .stats {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+    grid-gap: var(--spacing-16);
+
+    .wide {
+      grid-column-end: span 2;
+    }
+  }
+
+  .chart-download {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .settings {
+    width: 100%;
+    display: grid;
+    grid-gap: var(--spacing-8);
+    grid-template-columns: repeat(auto-fit, minmax(208px, 1fr));
+
+    .block {
+      background-color: var(--gray-20);
+      height: auto;
+    }
   }
 </style>
 
-<div class="explore">
-  {#if isLoading}
-    <div class="explore-loading-overlay"></div>
-  {/if}
+{#if isLoading}
+  <Loading />
+{/if}
 
-  <!-- Controls -->
-  <div class="explore-controls"></div>
-
-  <!-- Title -->
-  <div class="explore-header block">
+<Dashboard>
+  <div slot="map">
     <StaticMap location="{$location}" width="{500}" height="{500}" />
-    <div class="explore-header-title">
-      <div class="h3 block-title">
-        {$location.title}&nbsp; (<span class="block-title"
-          >{$location.geometry.coordinates[0]}°</span
-        >,&nbsp;
-        <span class="block-title">{$location.geometry.coordinates[1]}°</span>)
-      </div>
-      <div class="h4">
-        Distribution of daily <span class="block-title">{$climvar.title}s</span>
-        around
-        <span class="block-title">{$doyText} ({$doyRange})</span> from 1991-2020.
-      </div>
-      <div>
-        <Button size="small" icon="{Location16}" on:click="{loadLocation}">
-          Change Location
-        </Button>
-      </div>
-    </div>
   </div>
 
-  <!-- Stats -->
-  <div
-    class="explore-stats"
-    style="grid-template-columns: auto minmax(18rem, 1fr);"
-  >
-    <div class="block">
+  <div slot="title" class="block title">
+    <div class="h3">
+      {$location.title} ({$location.geometry.coordinates[0]}°, {$location
+        .geometry.coordinates[1]}°)
+    </div>
+    <div class="h4">
+      Distribution of daily <span class="annotate">{$climvar.title}s</span>
+      around <span class="annotate">{$doyText}</span> (<span class="annotate"
+        >{$doyRange}</span
+      >) from 1991-2020.
+    </div>
+    <Button size="small" icon="{Location16}" on:click="{loadLocation}">
+      Change Location
+    </Button>
+  </div>
+
+  <div slot="stats" class="stats">
+    <div class="block wide">
       {#if threshValid && threshProbability}
         <p>
-          A daily <span class="block-title">{$climvar.title}</span> of
-          <span class="block-title threshold">{$thresholdStore}°F</span>
-          around <span class="block-title">{$doyText}</span> is a
-          <span class="block-title">{threshProbability.label}</span>
-          event. Based on historical observations, the probability of exceeding
-          <span class="block-title threshold">{$thresholdStore}°F</span>
-          at least once between <span class="block-title">{$begin}</span> and
-          <span class="block-title">{$end}</span>
-          is <span class="block-title">{threshProbability.value}%</span>.
+          A daily <span class="annotate">{$climvar.title}</span> of
+          <span class="annotate threshold">{$thresholdStore}°F</span>
+          around <span class="annotate">{$doyText}</span> is a
+          <span class="annotate">{threshProbability.label}</span>
+          event. Based on Extreme Value Theory and historical observations (1991-2020),
+          the probability of daily
+          <span class="annotate">{$climvar.title}</span>
+          being {$extremesStore}er than
+          <span class="annotate threshold">{$thresholdStore}°F</span>
+          at least once between <span class="annotate">{$begin}</span> and
+          <span class="annotate">{$end}</span>
+          is {threshProbability.append}
+          <span class="annotate">{threshProbability.value}%</span>.
         </p>
         <p>
-          In the Baseline period (1991-2020), a temperature of <span
-            class="block-title threshold">{$thresholdStore} °F</span
+          In the Baseline Period (1991-2020), a daily <span class="annotate"
+            >{$climvar.title}</span
           >
-          was exceeded <span class="block-title">{threshExceedances}</span>
-          times between <span class="block-title">{$begin}</span> and
-          <span class="block-title">{$end}</span> for the graphic below.
+          {$extremesStore}er than
+          <span class="annotate threshold">{$thresholdStore} °F</span>
+          occurred <span class="annotate">{threshExceedances}</span>
+          times between <span class="annotate">{$begin}</span> and
+          <span class="annotate">{$end}</span>.
         </p>
         <ShowDefinition
           topics="{['extreme-event']}"
           title="What is an extreme event?"
-          cta="Extreme events"
+          cta="Extreme Events"
           on:define
         />
         <ShowDefinition
@@ -378,35 +432,33 @@
         />
       {/if}
     </div>
-
     <div class="block">
       {#if $baseline}
         <p>
           In the available record ({$hadisdDateRange}) between
-          <span class="block-title">{$begin}</span>
+          <span class="annotate">{$begin}</span>
           and
-          <span class="block-title">{$end}</span>:
+          <span class="annotate">{$end}</span>:
         </p>
         <p>
-          A <span class="block-title">Record Low</span> of
-          <span class="block-title">{$baseline.low.value}</span>
-          was observed on <span class="block-title">{$baseline.low.date}</span>
+          A <span class="annotate">Record Low</span> of
+          <span class="annotate">{$baseline.low.value}</span>
+          was observed on <span class="annotate">{$baseline.low.date}</span>
         </p>
         <p>
-          A <span class="block-title">Record High</span> of
-          <span class="block-title">{$baseline.high.value}</span>
-          was observed on <span class="block-title">{$baseline.high.date}</span>
+          A <span class="annotate">Record High</span> of
+          <span class="annotate">{$baseline.high.value}</span>
+          was observed on <span class="annotate">{$baseline.high.date}</span>
         </p>
       {/if}
     </div>
   </div>
 
-  <!-- Chart-->
-  <div class="explore-chart block">
+  <div slot="graphic" class="graphic block">
     <div class="chart-controls">
       {#if measuredLoading && !measured}
         <InlineLoading
-          description="Fetching recent observations from NOAA..."
+          description="Fetching recent observations from NCEI..."
         />
       {:else}
         <div class="measured-toggle">
@@ -433,8 +485,7 @@
       data="{histogramData}"
       labels="{histogramAnnotation}"
       threshold="{threshValid ? $thresholdStore : null}"
-      forecast="{showForecast ? forecast : null}"
-      measured="{showMeasured ? measured : null}"
+      xDomain="{xDomain}"
       yAxis="{{
         label: `% Days`,
         tickFormat: formatFn,
@@ -446,80 +497,99 @@
         units: `${$climvar.units.imperial}`,
       }}"
     />
-    <div class="chart-notes">
+    {#if showMeasured && measured}
+      <Observations
+        title="{'Recent Observations'}"
+        data="{measured}"
+        height="{240}"
+        y1="{80}"
+        xDomain="{xDomain}"
+        units="{$climvar.units.imperial}"
+      />
+    {/if}
+    {#if showForecast && forecast}
+      <Observations
+        title="{'Forecast'}"
+        data="{forecast}"
+        height="{180}"
+        y1="{showMeasured ? 320 : 80}"
+        xDomain="{xDomain}"
+        units="{$climvar.units.imperial}"
+      />
+    {/if}
+    <div class="chart-notes margin--v-8">
       <p>
         Source: Cal-Adapt. Data: {dataSourceTitles.join(", ")}.
       </p>
     </div>
-    <div class="chart-download">
+    <div class="chart-download margin--v-8">
       <ShowDefinition
         topics="{['chart']}"
-        title="About this graphic"
+        title="About the Chart"
+        cta="Explain Chart"
         on:define
       />
       <div>
         <Button size="small" icon="{Download16}" on:click="{loadDownload}">
           Download Chart
         </Button>
-        <Button
-          size="small"
-          icon="{Share16}"
-          cta="About this graphic"
-          on:click="{() => (showShare = true)}"
-        >
+        <Button size="small" icon="{Share16}" on:click="{loadShare}">
           Share
         </Button>
       </div>
     </div>
   </div>
 
-  <!-- Settings-->
-  <div class="explore-settings">
-    <div class="h4 block-title">Change Settings:</div>
-    <Accordion class="settings-list">
-      <AccordionItem open title="Select Climate Variable">
-        <SelectClimvar
-          selectedId="{$climvarStore}"
-          items="{climvarList}"
-          on:change="{changeClimvar}"
-        />
-        <ShowDefinition
-          on:define
-          topics="{['tasmax', 'tasmin']}"
-          title="Climate Variables"
-        />
-      </AccordionItem>
-      <AccordionItem open title="Choose Type of Extremes">
-        <RadioButtonGroup
-          orientation="vertical"
-          selected="high"
-          on:change="{changeExtremes}"
-        >
-          <RadioButton labelText="High Extremes" value="high" />
-          <RadioButton labelText="Low Extremes" value="low" />
-        </RadioButtonGroup>
-        <ShowDefinition
-          on:define
-          topics="{['extremes']}"
-          title="Type of Extremes"
-        />
-      </AccordionItem>
-      <AccordionItem open title="Choose Day of Year">
-        <SelectDayOfYear value="{$doyStore}" on:change="{changeDayOfYear}" />
-        <ShowDefinition on:define topics="{['doy']}" title="Day of Year" />
-      </AccordionItem>
-      <AccordionItem open title="Enter Threshold">
-        <NumberInput
-          invalid="{!threshValid}"
-          invalidText="{threshInvalidText}"
-          value="{$thresholdStore}"
-          on:change="{changeThreshold}"
-        />
-        <ShowDefinition on:define topics="{['threshold']}" title="Threshold" />
-      </AccordionItem>
-    </Accordion>
+  <div slot="settings" class="settings">
+    <div class="block">
+      <SelectClimvar
+        labelText="Select Climate Variable"
+        selectedId="{$climvarStore}"
+        items="{climvarList}"
+        on:change="{changeClimvar}"
+      />
+      <ShowDefinition
+        on:define
+        topics="{['tasmax', 'tasmin']}"
+        title="Climate Variables"
+      />
+    </div>
+    <div class="block">
+      <RadioButtonGroup
+        legendText="Choose Type of Extremes"
+        orientation="vertical"
+        selected="high"
+        on:change="{changeExtremes}"
+      >
+        <RadioButton labelText="High Extremes" value="high" />
+        <RadioButton labelText="Low Extremes" value="low" />
+      </RadioButtonGroup>
+      <ShowDefinition
+        on:define
+        topics="{['extremes']}"
+        title="Type of Extremes"
+      />
+    </div>
+    <div class="block">
+      <SelectDayOfYear
+        labelText="Choose Day of Year"
+        value="{$doyStore}"
+        on:change="{changeDayOfYear}"
+      />
+      <ShowDefinition on:define topics="{['doy']}" title="Day of Year" />
+    </div>
+    <div class="block">
+      <NumberInput
+        label="Enter Threshold"
+        invalid="{!threshValid}"
+        invalidText="{threshInvalidText}"
+        value="{$thresholdStore}"
+        on:change="{changeThreshold}"
+      />
+      <ShowDefinition on:define topics="{['threshold']}" title="Threshold" />
+    </div>
   </div>
-</div>
+</Dashboard>
 
 <svelte:component
   this="{ShareLink}"
