6b7eac8f6520eb5f5b1523872b1201d443facc2c
Shruti Mukhtyar <Shruti Mukhtyar>
2021-09-06 20:58:07 -0700

    Use carbon grid and classes

--- src/components/tools/Partials/Header.svelte
+++ src/components/tools/Partials/Header.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <style lang="scss">
-  .banner-tool {
-    padding: 2rem;
+  .banner {
     background: var(--gray-10);
     color: var(--text-01);
+    padding: 3rem 0;
 
     h1 {
       margin-top: var(--spacing-8);
@@ -22,13 +22,18 @@
       }
     }
   }
+
+  .banner :global(.lead a) {
+    color: var(--link-01);
+    text-decoration: none;
+  }
 </style>
 
 <!-- Banner -->
-<div class="banner-tool bg-gray-100">
+<div class="bleed banner">
   <div class="bx--grid">
     <div class="bx--row">
-      <div class="bx--col">
+      <div class="bx--col-lg-12 bx--col-md-8 bx--col-sm-4">
         <div class="title">
           {#if Array.isArray(iconPaths) && iconPaths.length}
             {#each iconPaths as icon}
