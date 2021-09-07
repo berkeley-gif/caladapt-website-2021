8d64ca9558eef2c44d829807d81f851af658a986
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-09-07 14:35:25 -0700

    Update tool description

--- src/routes/tools/extreme-weather/index.svelte
+++ src/routes/tools/extreme-weather/index.svelte
@@ -327,8 +327,8 @@
     <p class="lead">
       Explore extreme temperatures for past weather and present day. This tool
       provides data from 38 weather stations across California, utilizing a
-      quality-controlled dataset for hourly weather observations produced for
-      use by the energy sector (<a
+      quality-controlled dataset for hourly weather observations curated for use
+      by the energy sector (<a
         href="https://www.energy.ca.gov/sites/default/files/2021-05/CEC-500-2020-039.pdf"
         target="_blank">Doherty 2020</a
       >). Present day weather conditions are from NOAA.
