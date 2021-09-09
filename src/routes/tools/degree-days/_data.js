e8bd15ba0b799b8ee678cc50aca7eb5f97ed5c6a
Chris Henrick <chrishenrick@gmail.com>
2021-09-09 15:42:48 -0700

    added boilerplate for  modules: _data, _helpers, _store

---
+++ src/routes/tools/degree-days/_data.js
@@ -0,0 +1,208 @@
+// Node modules
+import { merge, rollup, sort, group } from "d3-array";
+
+// Helpers
+import config from "~/helpers/api-config";
+import {
+  handleXHR,
+  fetchData,
+  transformResponse,
+  pipe,
+  isLeapYear,
+} from "~/helpers/utilities";
+import { seriesList } from "./_helpers";
+
+const { apiEndpoint } = config.env.production;
+
+const fetchTimeseries = async ({ slug, params, method }) => {
+  const url = `${apiEndpoint}/series/${slug}/events/`;
+  const [response, error] = await handleXHR(fetchData(url, params, method));
+  if (error) {
+    throw new Error(error.message);
+  }
+  return response;
+};
+
+const addSeriesInfo = (series) => {
+  const item = seriesList.find((d) => d.key === series.key);
+  return { ...series, ...item, visible: true };
+};
+
+const buildEnvelope = (_data) => {
+  const dataMap = rollup(
+    _data,
+    (v) => v.map((i) => i.value),
+    (d) => d.date.getFullYear()
+  );
+  const dataArr = Array.from(dataMap);
+  return dataArr.map(([key, value]) => {
+    const sortedArr = sort(value);
+    return {
+      date: new Date(key, 11, 31),
+      min: sortedArr[0],
+      max: sortedArr[1],
+    };
+  });
+};
+
+// Converts precipitation data from inches/day to inches/year
+const convertToAnnual = (values) => {
+  return values.map((d) => {
+    if (isLeapYear(+d.date.getFullYear())) {
+      return { ...d, value: d.value * 366 };
+    }
+    return { ...d, value: d.value * 365 };
+  });
+};
+
+export async function addModel(config, params, method, modelId) {
+  const { climvarId, scenarioId } = config;
+  const slugs = [
+    `${climvarId}_year_${modelId}_historical`,
+    `${climvarId}_year_${modelId}_${scenarioId}`,
+  ];
+  const promises = slugs.map((slug) => {
+    return pipe(fetchTimeseries, transformResponse)({ slug, params, method });
+  });
+  return Promise.all(promises)
+    .then((results) => {
+      const values = merge(results);
+      if (values.length === 0) {
+        throw new Error("No Data");
+      }
+      const series = {
+        key: modelId,
+        type: "line",
+        values: climvarId === "pr" ? convertToAnnual(values) : values,
+      };
+      return addSeriesInfo(series);
+    })
+    .catch((error) => {
+      throw new Error(`${modelId}: ${error.message}`);
+    });
+}
+
+export async function getObserved(config, params, method) {
+  try {
+    const { climvarId } = config;
+    const slug = `${climvarId}_year_livneh`;
+    const promise = pipe(
+      fetchTimeseries,
+      transformResponse
+    )({ slug, params, method });
+    const values = await promise;
+    if (values.length === 0) {
+      throw new Error("No Data");
+    }
+    // Filter out data before 2007 for livneh
+    const filteredValues = values.filter((d) => +d.date.getFullYear() < 2007);
+
+    const observed = {
+      key: "observed",
+      type: "line",
+      values:
+        climvarId === "pr" ? convertToAnnual(filteredValues) : filteredValues,
+    };
+    return addSeriesInfo(observed);
+  } catch (error) {
+    throw new Error(`Observed: ${error.message}`);
+  }
+}
+
+export async function getModels(config, params, method) {
+  const { modelIds } = config;
+  const modelList = modelIds.split(",");
+  const modelPromise = modelList.map((modelId) =>
+    addModel(config, params, method, modelId)
+  );
+  const modelData = await Promise.all(modelPromise);
+  return modelData;
+}
+
+export async function getEnvelope(config, params, method) {
+  const { climvarId, scenarioId } = config;
+  const slugs = [
+    `${climvarId}_year_models-min_historical`,
+    `${climvarId}_year_models-min_${scenarioId}`,
+    `${climvarId}_year_models-max_historical`,
+    `${climvarId}_year_models-max_${scenarioId}`,
+  ];
+  const promises = slugs.map((slug) => {
+    return pipe(fetchTimeseries, transformResponse)({ slug, params, method });
+  });
+  return Promise.all(promises)
+    .then((results) => {
+      let arr;
+      if (climvarId === "pr") {
+        arr = convertToAnnual(merge(results));
+      } else {
+        arr = merge(results);
+      }
+      const values = buildEnvelope(arr, climvarId);
+      const series = {
+        key: scenarioId,
+        type: "area",
+        values,
+      };
+      return addSeriesInfo(series);
+    })
+    .catch((error) => {
+      throw new Error(`Envelope: ${error.message}`);
+    });
+}
+
+export function flattenData(_data) {
+  return _data.reduce((acc, series) => {
+    const seriesValues = series.values.map((d) => {
+      return {
+        ...d,
+        year: +d.date.getFullYear(),
+        key: series.key,
+        label: series.label,
+      };
+    });
+    acc.push(...seriesValues);
+    return acc;
+  }, []);
+}
+
+export function getDataByDate(_arr) {
+  return Array.from(
+    group(_arr, (d) => d.year),
+    ([year, values]) => {
+      const date = new Date(year, 11, 31);
+      const rows = values.map((d) => {
+        if (d.min) {
+          return {
+            key: d.key,
+            label: d.label,
+            value: [d["min"], d["max"]],
+          };
+        } else {
+          return {
+            key: d.key,
+            label: d.label,
+            value: d.value,
+          };
+        }
+      });
+      return { date, values: rows };
+    }
+  );
+}
+
+export function formatDataForExport(_arr) {
+  return _arr.map((item) => {
+    const row = {};
+    row.year = item.date.getFullYear();
+    item.values.forEach((d) => {
+      if (Array.isArray(d.value)) {
+        row[`${d.label} Min`] = d.value[0];
+        row[`${d.label} Max`] = d.value[1];
+      } else {
+        row[d.label] = d.value;
+      }
+    });
+    return row;
+  });
+}
---
+++ src/routes/tools/degree-days/_helpers.js
@@ -0,0 +1,80 @@
+import models from "~/helpers/climate-models";
+import scenarios from "~/helpers/climate-scenarios";
+import boundaries from "~/helpers/mapbox-layers";
+import climvars from "~/helpers/climate-variables";
+
+export const climvarList = climvars
+  .filter((d) => ["ccd", "hdd"].includes(d.id))
+  .map((d) => {
+    const title = `Annual Average ${d.label}`;
+    return { ...d, title };
+  });
+
+// List of series used in tool with additional props for
+// symbolyzing these lines
+const series = [
+  {
+    key: "observed",
+    label: "Observed",
+    color: "rgba(110, 110, 110, 1)",
+    mark: "line",
+  },
+  {
+    key: "rcp45",
+    label: "Modeled RCP 4.5 Range",
+    color: "rgba(218, 222, 225, 1)",
+    mark: "area",
+  },
+  {
+    key: "rcp85",
+    label: "Modeled RCP 8.5 Range",
+    color: "rgba(218, 222, 225, 1)",
+    mark: "area",
+  },
+];
+
+models.forEach((d) => {
+  let label;
+  if (d.text) {
+    label = `${d.id} (${d.text})`;
+  } else {
+    label = d.id;
+  }
+  series.push({
+    key: d.id,
+    label,
+    color: d.color,
+    mark: "line",
+  });
+});
+
+export const seriesList = series;
+
+export const boundaryList = boundaries
+  .filter((d) => d.metadata.group === "Boundaries")
+  .map((d) => {
+    let text;
+    if (d.id === "locagrid") {
+      text = "None";
+    } else {
+      text = d.metadata.title;
+    }
+    return { ...d, text };
+  });
+
+export const modelList = models.map((d) => {
+  let text;
+  if (d.text) {
+    text = `${d.id} (${d.text})`;
+  } else {
+    text = d.id;
+  }
+  return {
+    ...d,
+    text,
+  };
+});
+
+export const scenarioList = scenarios.filter((d) =>
+  ["rcp45", "rcp85"].includes(d.id)
+);
