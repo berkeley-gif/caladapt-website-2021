5b0a6eddc7a614c6e1617041861728679b7136dc
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-09-09 14:55:19 -0700

    Add metadata

--- content/tools/data.js
+++ content/tools/data.js
@@ -87,10 +87,31 @@ const tools = [
   {
     title: 'Extreme Heat',
     desc: `Projected frequency and duration of extreme heat days and warm nights.`,
-    categories: ['Precipitation'],
+    categories: ['Temperature'],
     slug: 'extreme-heat',
     icons: [
       'img/icons/sun.svg',
+    ],
+    datasets: [
+      {
+        slug: 'cb0c5f49-59d2-4f8e-8d9c-a047833f6081',
+        logo: 'scripps_100x100.png',
+      },
+      {
+        slug: '0887cba6-695c-403a-a757-674075799ea5',
+        logo: 'u_colorado.png',
+      },
+      {
+        slug: 'b8bee9f9-31c1-40d9-89a3-2dad814ba7da',
+        logo: 'gif_80x80.png'
+      }
+    ],
+    related: [
+      'extreme-weather','extreme-precipitation',
+    ],
+    resources: [
+      'California Adaptation Clearinghouse', 
+      "California's 4th Climate Change Assessment",
     ],
   },
   {
