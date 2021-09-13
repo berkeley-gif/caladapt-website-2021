9e79adb5589af0a421025c892a114d6852aabe8a
Shruti Mukhtyar <Shruti Mukhtyar>
2021-09-12 19:03:06 -0700

    Add prop labelText

--- src/components/tools/Settings/SelectClimvar.svelte
+++ src/components/tools/Settings/SelectClimvar.svelte
@@ -1,36 +1,23 @@
 <script>
-  import { onMount, createEventDispatcher } from "svelte";
-  import { Select, SelectItem, SelectSkeleton } from "carbon-components-svelte";
+  import { createEventDispatcher } from "svelte";
+  import { Select, SelectItem } from "carbon-components-svelte";
 
   export let selectedId;
   export let items;
+  export let labelText = "Select Climate Variable";
 
   const dispatch = createEventDispatcher();
 
   let selected = items.find((d) => d.id === selectedId);
-  let ready = false;
 
   function change(e) {
     selected = items.find((d) => d.id === e.detail);
     dispatch("change", selected);
   }
-
-  onMount(() => {
-    ready = true;
-    dispatch("ready");
-  });
 </script>
 
-{#if ready}
-  <Select
-    labelText="Select Climate Variable"
-    selected="{selectedId}"
-    on:change="{change}"
-  >
-    {#each items as opt}
-      <SelectItem value="{opt.id}" text="{opt.label}" />
-    {/each}
-  </Select>
-{:else}
-  <SelectSkeleton />
-{/if}
+<Select labelText="{labelText}" selected="{selectedId}" on:change="{change}">
+  {#each items as opt}
+    <SelectItem value="{opt.id}" text="{opt.label}" />
+  {/each}
+</Select>
