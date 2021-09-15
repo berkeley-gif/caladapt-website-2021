4cca2a7433979d20aad079b71f714b2490297dfb
Chris Henrick <chrishenrick@gmail.com>
2021-09-15 11:48:08 -0700

    updated SelectMonth component: - include option for multi-select

--- src/components/tools/Settings/SelectMonth.svelte
+++ src/components/tools/Settings/SelectMonth.svelte
@@ -1,10 +1,16 @@
 <script>
   import { onMount, createEventDispatcher } from "svelte";
-  import { Select, SelectItem, SelectSkeleton } from "carbon-components-svelte";
+  import {
+    MultiSelect,
+    Select,
+    SelectItem,
+    SelectSkeleton,
+  } from "carbon-components-svelte";
 
   export let selectedId;
   export let items;
   export let title = "Select Month";
+  export let multi = false;
 
   const dispatch = createEventDispatcher();
 
@@ -14,6 +20,10 @@
   function change(e) {
     selected = items.find((d) => d.id === parseInt(e.detail));
     dispatch("change", selected);
+  }
+
+  function select(e) {
+    dispatch("change", e.detail.selectedIds);
   }
 
   onMount(() => {
@@ -29,16 +39,28 @@
 </style>
 
 {#if ready}
-  <Select
-    class="month-select"
-    labelText="{title}"
-    selected="{selectedId}"
-    on:change="{change}"
-  >
-    {#each items as opt}
-      <SelectItem value="{opt.id}" text="{opt.label}" />
-    {/each}
-  </Select>
+  {#if multi}
+    <MultiSelect
+      class="month-select"
+      titleText="{title}"
+      label="Select months"
+      selectedIds="{selectedId}"
+      items="{items}"
+      on:select="{select}"
+      sortItem="{() => {}}"
+    />
+  {:else}
+    <Select
+      class="month-select"
+      labelText="{title}"
+      selectedIds="{selectedId}"
+      on:change="{change}"
+    >
+      {#each items as opt}
+        <SelectItem value="{opt.id}" text="{opt.label}" />
+      {/each}
+    </Select>
+  {/if}
 {:else}
   <SelectSkeleton />
 {/if}
