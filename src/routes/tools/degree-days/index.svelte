f8086cbc7dd779d009bb569bd8b2c15dd5fee509
Chris Henrick <chrishenrick@gmail.com>
2021-09-14 17:09:07 -0700

    renamed ExploreData to _ExploreData

--- src/routes/tools/degree-days/index.svelte
+++ src/routes/tools/degree-days/index.svelte
@@ -66,7 +66,7 @@
   import { getFeature, reverseGeocode } from "~/helpers/geocode";
 
   // Components
-  import ExploreData from "./ExploreData.svelte";
+  import ExploreData from "./_ExploreData.svelte";
   import {
     Header,
     About,
