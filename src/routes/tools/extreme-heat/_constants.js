6a0a4c2ed10c75672f9ac15abf72556ff3ef9722
Shruti Mukhtyar <Shruti Mukhtyar>
2021-09-20 20:01:22 -0700

    Make indicator list consant

--- src/routes/tools/extreme-heat/_constants.js
+++ src/routes/tools/extreme-heat/_constants.js
@@ -1,6 +1,51 @@
+import {
+  LineAreaChart,
+  ScatterChart,
+  HeatmapChart,
+} from "~/components/tools/Charts";
+import { RangeAvg, MonthsCount } from "~/components/tools/Stats";
+
 export const TOOL_SLUG = "extreme-heat";
 
 export const CLIMATE_VARIABLES = ["tasmax", "tasmin"];
+export const CLIMATE_INDICATORS = [
+  {
+    id: "frequency",
+    label: "Frequency",
+    title: "Number of Extreme Heat Days per Year",
+    units: "days/yr",
+    decimals: 0,
+    chartComponent: LineAreaChart,
+    statsComponent: RangeAvg,
+  },
+  {
+    id: "timing",
+    label: "Timing",
+    title: "Timing of Extreme Heat Days per Year",
+    units: "",
+    decimals: 0,
+    chartComponent: HeatmapChart,
+    statsComponent: MonthsCount,
+  },
+  {
+    id: "duration",
+    label: "Duration",
+    title: "Longest Stretch of Consecutive Extreme Heat Days per Year",
+    units: "days/yr",
+    decimals: 1,
+    chartComponent: ScatterChart,
+    statsComponent: RangeAvg,
+  },
+  {
+    id: "waves",
+    label: "Heat Waves",
+    title: "Number of Heat Wave Events per Year",
+    units: "events/yr",
+    decimals: 1,
+    chartComponent: ScatterChart,
+    statsComponent: RangeAvg,
+  },
+];
 
 export const DEFAULT_CLIMATE_VARIABLE = CLIMATE_VARIABLES[0];
 export const DEFAULT_CLIMATE_INDICATOR = "frequency";
