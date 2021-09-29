aa81fd8d06f3146fc6a3af2c33e24873f6ee0b7c
Chris Henrick <161748+clhenrick@users.noreply.github.com>
2021-09-29 12:46:00 -0700

    Update deploy script (#71)
    * add --transfer flag for disabling rysnc to server
    
    * updated usage

--- scripts/deploy.mjs
+++ scripts/deploy.mjs
@@ -2,7 +2,19 @@
 
 /**
  * deploy.js
- * Handles transpiling and deploying code based on environment configuration.
+ * Handles transpiling and deploying source code.
+ *
+ * Available options:
+ * --help: prints script usage instructions.
+ * --location: (required) determines env variables and where compiled
+ *    assets should be rsync'd to.
+ * --transfer: (optional) disables rsync when set to false.
+ *
+ * Examples (assumes script is being run from root of repository):
+ *
+ * npm run deploy -- --help
+ * npm run deploy -- --location=dev
+ * npm run deploy -- --location=beta --transfer=false
  */
 
 import { $ } from "zx";
@@ -13,12 +25,14 @@ const BASE_URI = "cal-adapt.org";
 const DEPLOY_URI = `api.${BASE_URI}`;
 const DIR = "/var/www/";
 
+let allowTransfer = true;
+
 main();
 
 async function main() {
   // zx shorthand for parsing script arguments in the form of `--foo=bar`
   // eslint-disable-next-line no-undef
-  const { location, help } = argv;
+  const { location, help, transfer } = argv;
 
   if (help) {
     return await usage();
@@ -28,6 +42,10 @@ async function main() {
     return await usage("Missing argument for --location");
   }
 
+  if (transfer === "false") {
+    allowTransfer = false;
+  }
+
   await handleLocation(location);
 }
 
@@ -35,7 +53,7 @@ async function usage(message) {
   if (message) {
     console.log(message);
   }
-  console.log("Usage: `zx deploy.mjs --location=<deploy location>`");
+  console.log("Usage: zx deploy.mjs --location=<string> --transfer=<boolean>");
   await $`exit 0`;
 }
 
@@ -49,10 +67,15 @@ async function sapperExport() {
 }
 
 async function transfer(subdomain) {
-  const domain = subdomain ? `${subdomain}.${BASE_URI}` : BASE_URI;
-  await $`rsync \
-    -avz ${SAPPER_EXPORT_DIR} \
-    ${USER}@${DEPLOY_URI}:${DIR}${domain}/`;
+  if (allowTransfer) {
+    const domain = subdomain ? `${subdomain}.${BASE_URI}` : BASE_URI;
+    await $`rsync \
+      -avz ${SAPPER_EXPORT_DIR} \
+      ${USER}@${DEPLOY_URI}:${DIR}${domain}/`;
+  } else {
+    console.log("transfer disabled via --transfer option, exiting");
+    return Promise.resolve();
+  }
 }
 
 async function deployBeta() {
