e732719b9917c36d8e60b1c288fcb025db75d2ff
Chris Henrick <chrishenrick@gmail.com>
2021-10-07 08:53:08 -0700

    updated Dashboard component to optionally use tabs

--- src/components/tools/Partials/Dashboard.svelte
+++ src/components/tools/Partials/Dashboard.svelte
@@ -1,3 +1,24 @@
+<script>
+  import { createEventDispatcher, onMount } from "svelte";
+  import { Tabs, Tab, TabContent } from "carbon-components-svelte";
+
+  export let useTabs = false;
+
+  const activeTabDispatcher = createEventDispatcher();
+
+  let activeTab = 0;
+
+  $: activeTab, dispatchActiveTab();
+
+  onMount(() => {
+    if (useTabs) dispatchActiveTab();
+  });
+
+  function dispatchActiveTab() {
+    activeTabDispatcher("tabChange", activeTab ? "chart" : "map");
+  }
+</script>
+
 <style lang="scss">
   .dashboard {
     display: flex;
@@ -18,6 +39,24 @@
   .is-sticky {
     top: -1px;
   }
+
+  // overrides for tab content area
+  :global(.bx--tab-content) {
+    padding: 0;
+  }
+
+  :global(.bx--tabs__nav-link) {
+    font-size: 0.875rem;
+  }
+
+  :global(.bx--grid--full-width) {
+    padding: 0;
+  }
+
+  div[slot="content"] {
+    width: 100%;
+  }
+  // end overrides for tab content area
 
   @media (max-width: 1000px) {
     .dashboard {
@@ -50,27 +89,79 @@
 
 <div class="dashboard">
   <div class="bx--grid bx--grid--narrow content">
-    <div class="bx--row margin--v-16">
-      <div
-        class="bx--col-lg-4 bx--col-md-2 bx--col-sm-4 bx--aspect-ratio bx--aspect-ratio--1x1"
-      >
-        <slot name="map">map</slot>
+    {#if useTabs}
+      <div class="bx--row">
+        <Tabs type="container" bind:selected="{activeTab}">
+          <Tab label="Map" />
+          <Tab label="Chart" />
+          <div slot="content">
+            <TabContent>
+              <div class="bx--grid bx--grid--full-width">
+                <div class="bx--row bx--no-gutter margin--v-16">
+                  <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
+                    <slot name="tab_content_slippy_map"
+                      >tab_content_slippy_map</slot
+                    >
+                  </div>
+                </div>
+              </div>
+            </TabContent>
+
+            <TabContent>
+              <div class="bx--grid bx--grid--full-width">
+                <div class="bx--row margin--v-16">
+                  <div
+                    class="bx--col-lg-4 bx--col-md-2 bx--col-sm-4 bx--aspect-ratio bx--aspect-ratio--1x1"
+                  >
+                    <slot name="tab_content_map">tab_content_map</slot>
+                  </div>
+                  <div class="bx--col-lg-12 bx--col-md-6 bx--col-sm-4">
+                    <slot name="tab_content_title">tab_content_title</slot>
+                  </div>
+                </div>
+
+                <div class="bx--row bx--no-gutter margin--v-16">
+                  <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
+                    <slot name="tab_content_stats">tab_content_stats</slot>
+                  </div>
+                </div>
+
+                <div class="bx--row bx--no-gutter margin--v-16">
+                  <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
+                    <slot name="tab_content_graphic">tab_content_graphic</slot>
+                  </div>
+                </div>
+              </div>
+            </TabContent>
+          </div>
+        </Tabs>
       </div>
-      <div class="bx--col-lg-12 bx--col-md-6 bx--col-sm-4">
-        <slot name="title">title</slot>
+    {:else}
+      <div class="bx--row margin--v-16">
+        <div
+          class="bx--col-lg-4 bx--col-md-2 bx--col-sm-4 bx--aspect-ratio bx--aspect-ratio--1x1"
+        >
+          <slot name="map">map</slot>
+        </div>
+        <div class="bx--col-lg-12 bx--col-md-6 bx--col-sm-4">
+          <slot name="title">title</slot>
+        </div>
       </div>
-    </div>
-    <div class="bx--row margin--v-16">
-      <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
-        <slot name="stats">stats</slot>
+
+      <div class="bx--row margin--v-16">
+        <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
+          <slot name="stats">stats</slot>
+        </div>
       </div>
-    </div>
-    <div class="bx--row margin--v-16">
-      <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
-        <slot name="graphic">graphic</slot>
+
+      <div class="bx--row margin--v-16">
+        <div class="bx--col-lg-16 bx--col-md-8 bx--col-sm-4">
+          <slot name="graphic">graphic</slot>
+        </div>
       </div>
-    </div>
+    {/if}
   </div>
+
   <aside class="bx--grid bx--grid--condensed settings">
     <div class="is-sticky">
       <div class="bx--row margin--v-16">
