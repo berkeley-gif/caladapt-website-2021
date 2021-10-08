68d258022acc0e0d43fa4f1214bafd914bae7a68
Chris Henrick <chrishenrick@gmail.com>
2021-10-07 17:32:54 -0700

    added some settings for map & chart in ExploreData

--- src/routes/tools/snowpack/_constants.js
+++ src/routes/tools/snowpack/_constants.js
@@ -2,3 +2,6 @@ export const DEFAULT_CLIMVAR = "swe";
 export const DEFAULT_SELECTED_MONTH = 3;
 
 export const TOOL_SLUG = "snowpack";
+
+export const LEARN_MORE_SELECT_MONTH = `<p>TODO: something about why selecting a single
+month for Snowpack.</p>`;
