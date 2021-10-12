52819e985c03ee911f5efa9f74e6e95459c29dcd
Chris Henrick <chrishenrick@gmail.com>
2021-10-12 10:21:57 -0700

    added constant for months list, one indexed

--- src/routes/tools/snowpack/_constants.js
+++ src/routes/tools/snowpack/_constants.js
@@ -1,3 +1,6 @@
+import { range } from "d3-array";
+import { timeFormat } from "d3-time-format";
+
 export const DEFAULT_CLIMVAR = "swe";
 export const DEFAULT_CENTER = [-119.96, 38.9];
 export const DEFAULT_SELECTED_MONTH = 3;
@@ -13,3 +16,8 @@ export const TIME_DURATIONS = [
   { id: 10, label: "Ten years" },
   { id: 5, label: "Five years " },
 ];
+
+export const MONTHS_LIST_ONE_INDEXED = range(0, 12).map((d) => ({
+  id: d + 1,
+  label: timeFormat("%B")(new Date(new Date().getFullYear(), d, 1, 0, 0, 0)),
+}));
