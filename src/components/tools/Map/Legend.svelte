5efe0fa25082b1967f3504af98b0dd7862f29875
Chris Henrick <chrishenrick@gmail.com>
2021-10-12 16:32:13 -0700

    updated Map/Legend component: - use position absolute to place within map frame - styling updates

--- src/components/tools/Map/Legend.svelte
+++ src/components/tools/Map/Legend.svelte
@@ -1,33 +1,49 @@
 <script>
   import { scaleBand, scaleOrdinal } from "d3-scale";
 
-  export let width = 100;
-  export let height = 360;
-  export let margin = { top: 16, left: 8, right: 8, bottom: 8 };
+  export let width = 80;
+  export let height = 240;
+  export let margin = { top: 8, left: 0, right: 8, bottom: 0 };
   export let title = "Legend";
   export let ramp = [];
   export let values = [];
+  export let padding = 0.5;
+
+  const rectWidth = 30;
 
   $: yPos = scaleBand()
     .domain(values)
     .range([height - margin.bottom, margin.top])
     .paddingInner(0.2);
+
   $: color = scaleOrdinal().domain(values).range(ramp);
 </script>
 
 <style>
+  div {
+    position: absolute;
+    right: 0.5rem;
+    bottom: 0.5rem;
+    width: var(--width, 100%);
+    padding: var(--padding);
+    background-color: var(--white, #fff);
+    border: 1px solid var(--gray-40);
+    box-sizing: content-box;
+  }
+
   svg {
     max-width: 100%;
     height: auto;
-    border: 1px solid #000;
   }
 
+  p,
   text {
     font-size: 0.875rem;
     fill: var(--gray-80, #333);
   }
 
-  .legend--title-text {
+  p {
+    margin: 0;
     font-weight: 600;
   }
 
@@ -36,21 +52,26 @@
   }
 </style>
 
-<svg viewBox="0 0 {width} {height}">
-  <text class="legend--title-text" y="{margin.top}" x="{margin.left}"
-    >{title}</text
+<div style="--width:{width}px; --padding:{padding}rem;">
+  <p>{title}</p>
+  <svg
+    viewBox="0 0 {width} {height}"
+    role="presentation"
+    aria-label="map legend"
   >
-  <g transform="{`translate(${margin.left}, ${margin.top})`}">
-    {#each values as v, i (`${v}`)}
-      <g transform="{`translate(0, ${yPos(v)})`}">
-        <rect
-          fill="{color(v)}"
-          x="0"
-          y="0"
-          width="30"
-          height="{yPos.bandwidth()}"></rect>
-        <text class="legend--item-text" x="34" y="12">{v}</text>
-      </g>
-    {/each}
-  </g>
-</svg>
+    <g transform="{`translate(${margin.left}, ${0})`}">
+      {#each values as v, i (`${v}`)}
+        <g transform="{`translate(0, ${yPos(v)})`}">
+          <rect
+            fill="{color(v)}"
+            x="0"
+            y="0"
+            width="{rectWidth}"
+            height="{yPos.bandwidth()}"
+            stroke="var(--gray-40)"></rect>
+          <text class="legend--item-text" x="{rectWidth + 4}" y="12">{v}</text>
+        </g>
+      {/each}
+    </g>
+  </svg>
+</div>
