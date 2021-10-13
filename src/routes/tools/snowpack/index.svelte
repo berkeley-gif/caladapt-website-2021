a51e7c2978f562747c89ac96cf4bf741fdf88f27
Chris Henrick <chrishenrick@gmail.com>
2021-10-13 16:12:29 -0700

    add copy for about section

--- src/routes/tools/snowpack/index.svelte
+++ src/routes/tools/snowpack/index.svelte
@@ -243,7 +243,19 @@
       datasets="{datasets}"
       on:datasetLoaded="{(e) => datasetStore.set(e.detail)}"
     >
-      <div slot="description"></div>
+      <div slot="description">
+        <p>
+          If heat-trapping emissions continue unabated, more precipitation will
+          fall as rain instead of snow, and the snow that does fall will melt
+          earlier, reducing the Sierra Nevada spring snowpack by as much as 70
+          to 90 percent. How much snowpack will be lost depends in part on
+          future precipitation patterns, the projections for which remain
+          uncertain. However, even under wetter climate projections, the loss of
+          snowpack would pose challenges to water managers, hamper hydropower
+          generation, and nearly eliminate skiing and other snow-related
+          recreational activities.
+        </p>
+      </div>
     </About>
   </div>
 
