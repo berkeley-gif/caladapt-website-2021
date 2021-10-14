fee077b4202b218d25f4dbe0e22f3be974cfbb98
Eric Lehmer <elehmer@berkeley.edu>
2021-10-14 14:05:11 -0700

    Add Embed partial compotent for iframing tools

---
+++ src/partials/Embed.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let path;
+  export let title;
+
+  $: src = `https://v2.cal-adapt.org/${path}_iframe`;
+</script>
+
+<style>
+  .iframe {
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
+
+<iframe
+  class="iframe"
+  src="{src}"
+  title="{title}"
+  frameborder="0"
+  allow="fullscreen"></iframe>
