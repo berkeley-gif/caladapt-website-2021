b5722fd5886fc5518586b62e9cb47b7b163864af
Shruti Mukhtyar <Shruti Mukhtyar>
2021-10-19 08:52:54 -0700

    move note to a separate component

---
+++ src/components/tools/Stats/StatAbout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { Modal } from "carbon-components-svelte";
+
+  export let group;
+  export let period;
+  export let models;
+  export let open = false;
+
+  $: groupLabel = group.id.includes("model")
+    ? `${models.join(", ")} models`
+    : `${group.label} data`;
+  $: note = `Summary statistics are calculated using values between ${period.start}
+    and ${period.end} from ${groupLabel}.`;
+</script>
+
+<Modal
+  id="stats-about"
+  size="sm"
+  passiveModal
+  modalHeading="Summary Statistics"
+  bind:open
+  on:open
+  on:close="{() => (open = false)}"
+>
+  <div>
+    {note}
+  </div>
+</Modal>
