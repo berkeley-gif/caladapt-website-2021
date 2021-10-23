67f90b1e64948bc1764ee022489556ddc4493b40
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-10-22 17:42:48 -0700

    Remove unused components

--- src/components/tools/Stats/index.js
+++ src/components/tools/Stats/index.js
@@ -1,4 +1,2 @@
-export { default as MinMaxAvg } from "./MinMaxAvg.svelte";
 export { default as RangeAvg } from "./RangeAvg.svelte";
 export { default as MonthsCount } from "./MonthsCount.svelte";
-export { default as ValuesList } from "./ValuesList.svelte";
