8546270b0a13c60dde8b81738758f4f263bae2ce
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-10-25 10:40:40 -0700

    list any groups or periods users can select

---
+++ src/components/tools/Stats/StatOptions.svelte
@@ -0,0 +1,125 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import { range } from "d3-array";
+  import {
+    RadioButtonGroup,
+    RadioButton,
+    ComboBox,
+    Modal,
+  } from "carbon-components-svelte";
+
+  export let groupList;
+  export let periodList;
+  export let group;
+  export let period;
+  export let dateRange;
+  export let open = false;
+
+  const dispatch = createEventDispatcher();
+
+  // Props for binding to RadioButtonGroup
+  let selectedGroup = group.id;
+  let selectedPeriod = period.id;
+
+  // Props and functions for defining a custom period
+  let startYear_selectedIndex = -1;
+  let endYear_selectedIndex = -1;
+  let filteredItems;
+
+  const formatSelected = (i) => (items[i] ? +items[i].text : "N/A");
+
+  function updateLinkedList() {
+    if (startYear_selectedIndex < 0) {
+      filteredItems = items;
+      endYear_selectedIndex = -1;
+    } else {
+      filteredItems = items.slice(startYear_selectedIndex + 1);
+    }
+  }
+
+  $: yearsList = range(dateRange[0], dateRange[1], 1);
+  $: items = yearsList.map((d) => ({ id: d, text: `${d}` }));
+  $: startYear_selectedIndex, updateLinkedList();
+
+  // If user confirms changes, dispatch change event with selected groupd and period
+  // If user has selected a custom period, create a new period object
+  function update() {
+    group = groupList.find(({ id }) => id === selectedGroup);
+    if (selectedPeriod === "custom") {
+      const start = formatSelected(startYear_selectedIndex);
+      const end = formatSelected(endYear_selectedIndex) + 1;
+      period = {
+        id: "custom",
+        label: `${start}-${end}`,
+        start,
+        end,
+      };
+    } else {
+      period = periodList.find(({ id }) => id === selectedPeriod);
+    }
+    dispatch("change", { group, period });
+  }
+</script>
+
+<style>
+  .years-select {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 0.5rem;
+  }
+</style>
+
+<Modal
+  primaryButtonText="Confirm"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary="{() => (open = false)}"
+  bind:open
+  modalHeading="Change Stats"
+  on:submit="{update}"
+  on:close="{() => dispatch('cancel')}"
+>
+  <h5>Select Group</h5>
+  <RadioButtonGroup bind:selected="{selectedGroup}">
+    {#each groupList as { id, label }}
+      <RadioButton labelText="{label}" value="{id}" />
+    {/each}
+  </RadioButtonGroup>
+
+  <h5>Select Time Period</h5>
+  <RadioButtonGroup bind:selected="{selectedPeriod}">
+    {#each periodList as { id, label }}
+      <RadioButton labelText="{label}" value="{id}" />
+    {/each}
+    <RadioButton labelText="Enter custom year range" value="custom" />
+  </RadioButtonGroup>
+
+  {#if selectedPeriod === "custom"}
+    <div class="years-select">
+      <ComboBox
+        bind:selectedIndex="{startYear_selectedIndex}"
+        titleText="Start"
+        placeholder="Select start year"
+        items="{items}"
+      />
+      <ComboBox
+        bind:selectedIndex="{endYear_selectedIndex}"
+        titleText="End"
+        placeholder="Select end year"
+        items="{filteredItems}"
+      />
+    </div>
+  {/if}
+
+  <p style="margin-top:1.5rem;">
+    <strong
+      >Scientists recommend looking at a period of at least a few decades like a
+      30 year period.</strong
+    >
+  </p>
+  <p>
+    Future climate projections express natural climate variability. If you
+    analyze just a few years of a future climate projection, you might happen to
+    select years that are anomalous. Analyzing a longer time selected gives you
+    a better sense of overall future conditions.
+  </p>
+</Modal>
