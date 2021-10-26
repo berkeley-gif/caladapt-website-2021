8d39de1b9ca9a0cd7181f99b1bfd0b7688e27cd7
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-10-26 10:08:28 -0700

    Set datastore to empty on error

--- src/routes/tools/annual-averages/index.svelte
+++ src/routes/tools/annual-averages/index.svelte
@@ -137,14 +137,13 @@
         boundary: $boundary,
         imperial: true,
       });
-      /*      const envelope = await getEnsemble(config, params, method);
+      const envelope = await getEnsemble(config, params, method);
       const observed = await getObserved(config, params, method);
-      const modelsData = await getModels(config, params, method);*/
-      //dataStore.set([...envelope, ...observed, ...modelsData]);
-      dataStore.set([]);
+      const modelsData = await getModels(config, params, method);
+      dataStore.set([...envelope, ...observed, ...modelsData]);
     } catch (err) {
-      // TODO: notify user of error
       console.log("updateData", err);
+      dataStore.set([]);
       notifier.error("Error", err, 2000);
     }
   }
@@ -172,6 +171,7 @@
       })
       .catch((error) => {
         console.log("init error", error);
+        dataStore.set([]);
         notifier.error(
           "Unable to Load Tool",
           "Sorry! Something's probably wrong at our end. Try refereshing your browser. If you still see an error please contact us at support@cal-adapt.org.",
