df3af5faa8df6a9d947ffe12c8979da3bf1fea57
Chris Henrick <chrishenrick@gmail.com>
2021-10-27 09:48:28 -0700

    group map legend props in WildfireMap

--- src/routes/tools/wildfire/_WildfireMap.svelte
+++ src/routes/tools/wildfire/_WildfireMap.svelte
@@ -18,29 +18,36 @@
   export let climvarId;
   export let stateBoundary;
 
-  const height = 34;
-  const width = 280;
-  const scaleType = "continuous";
-  const noDataColor = "rgba(51,51,51,0.8)";
-  const rectWidth = "1.25rem";
-  const rectHeight = "0.75rem";
-
-  let title;
-  let values;
-  let ramp;
-  let tickValues;
+  let legendProps = {
+    height: 34,
+    width: 280,
+    scaleType: "continuous",
+    noDataColor: "rgba(51,51,51,0.8)",
+    rectWidth: "1.25rem",
+    rectHeight: "0.75rem",
+    title: "",
+    values: [],
+    ramp: [],
+    tickValues: [],
+  };
 
   $: if (climvarId === "fire") {
-    title = "Area burned (hectares)";
-    values = colorValuesFire;
-    ramp = colorRampFire;
+    legendProps = {
+      ...legendProps,
+      title: "Area burned (hectares)",
+      values: colorValuesFire,
+      ramp: colorRampFire,
+      tickValues: colorValuesFire.slice(),
+    };
   } else {
-    title = "Decadal wildfire probability";
-    values = colorValuesFireprob;
-    ramp = colorRampFireprob;
+    legendProps = {
+      ...legendProps,
+      title: "Decadal wildfire probability",
+      values: colorValuesFireprob,
+      ramp: colorRampFireprob,
+      tickValues: colorValuesFireprob.slice(),
+    };
   }
-
-  $: tickValues = values.slice();
 </script>
 
 <Map>
@@ -50,18 +57,5 @@
     overlay="{imgOverlayPath}"
     coordinates="{DEFAULT_OVERLAY_BOUNDS}"
   />
-  <Legend
-    {...{
-      title,
-      ramp,
-      values,
-      scaleType,
-      width,
-      height,
-      tickValues,
-      noDataColor,
-      rectWidth,
-      rectHeight,
-    }}
-  />
+  <Legend {...legendProps} />
 </Map>
