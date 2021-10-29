265decb26440d95272a0d0307c7f3e423906c29f
Chris Henrick <chrishenrick@gmail.com>
2021-10-29 10:44:14 -0700

    cards anchor elements use sapper prefetch

--- src/components/cards/CardTitle.svelte
+++ src/components/cards/CardTitle.svelte
@@ -32,13 +32,13 @@
 </style>
 
 {#if headingLevel === 3}
-  <h3><a href="{linkPath}">{titleText}</a></h3>
+  <h3><a sapper:prefetch href="{linkPath}">{titleText}</a></h3>
 {:else if headingLevel === 4}
-  <h4><a href="{linkPath}">{titleText}</a></h4>
+  <h4><a sapper:prefetch href="{linkPath}">{titleText}</a></h4>
 {:else if headingLevel === 5}
-  <h5><a href="{linkPath}">{titleText}</a></h5>
+  <h5><a sapper:prefetch href="{linkPath}">{titleText}</a></h5>
 {:else if headingLevel === 6}
-  <h6><a href="{linkPath}">{titleText}</a></h6>
+  <h6><a sapper:prefetch href="{linkPath}">{titleText}</a></h6>
 {:else}
-  <h2><a href="{linkPath}">{titleText}</a></h2>
+  <h2><a sapper:prefetch href="{linkPath}">{titleText}</a></h2>
 {/if}
