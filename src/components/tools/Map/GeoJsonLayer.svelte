125e5c96956b484b90c9518913b992aeb2dbfe81
Chris Henrick <chrishenrick@gmail.com>
2021-10-28 17:04:27 -0700

    GeoJsonLayer component accepts url for pattern fill image

--- src/components/tools/Map/GeoJsonLayer.svelte
+++ src/components/tools/Map/GeoJsonLayer.svelte
@@ -8,16 +8,22 @@
   export let styleProps = {
     "fill-color": "#333",
     "fill-opacity": 0.8,
+    "fill-pattern": "pattern",
   };
   export let layout = {
     visibility: "visible",
   };
   export let beforeId = undefined;
+  export let patternFillURL;
 
   const { getMap } = getContext(contextKey);
   const map = getMap();
 
   $: sourceName = `${layerName}-source`;
+
+  $: if (patternFillURL) {
+    loadMapImage(patternFillURL).catch(console.warn);
+  }
 
   $: if (data && typeof data === "object") {
     map.addSource(sourceName, {
@@ -35,6 +41,19 @@
       },
       beforeId
     );
+  }
+
+  function loadMapImage(url) {
+    return new Promise((resolve, reject) => {
+      map.loadImage(url, (error, image) => {
+        if (error) {
+          reject(error);
+        } else {
+          map.addImage("pattern", image);
+          resolve(image);
+        }
+      });
+    });
   }
 
   onDestroy(() => {
