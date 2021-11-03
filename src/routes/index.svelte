6ee51afafb59809472952062075288de1f88accf
Chris Henrick <chrishenrick@gmail.com>
2021-11-03 12:11:20 -0700

    Homepage sets Banner title font size & weight

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,48 +36,8 @@
 </script>
 
 <style lang="scss">
-  .banner {
-    --banner-child-v-spacing: 2.5rem;
-    background-image: url(/img/banners/yosemite_1600x540.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-
-    @media (max-width: 672px) {
-      background-image: url(/img/banners/yosemite_700x800.jpg);
-    }
-  }
-
-  .banner--icons {
-    padding: 0.5rem 1rem;
-    gap: 0.25rem;
-    margin-bottom: var(--banner-child-v-spacing);
-  }
-
-  .banner--content {
-    > * + * {
-      margin-top: var(--banner-child-v-spacing);
-    }
-
-    .title-container * {
-      display: inline;
-    }
-
-    .title-container p {
-      max-width: 50ch;
-    }
-
-    p.lead {
-      font-weight: 400;
-      max-width: 56ch;
-      margin-bottom: 0;
-    }
-
-    .title {
-      font-size: 2rem;
-    }
-  }
-
   .btn-container {
+    margin-top: 2.5rem;
     text-transform: uppercase;
   }
 
@@ -104,58 +64,28 @@
     needs."
   bannerImg="/img/banners/yosemite_1600x540.jpg"
   bannerImgMobile="/img/banners/yosemite_700x800.jpg"
-  overlayOpacity="{0.5}"
+  overlayOpacity="{0.6}"
+  titleFontSize="2rem"
+  titleFontWeight="400"
 >
-  <div class="bx--row banner--icons" aria-hidden="true" slot="icons">
+  <div
+    class="bx--row bx--offset-lg-2 banner--icons"
+    aria-hidden="true"
+    slot="icons"
+  >
     {#each icons as icon}
       <div class="bx--col-1">
         <div class="icon">{@html icon}</div>
       </div>
     {/each}
   </div>
+
   <div class="btn-container" slot="button">
     <Button icon="{ArrowRight16}" href="/about" sapper:prefetch
       >More about Cal-Adapt</Button
     >
   </div>
 </Banner>
-
-<!-- <section class="bleed banner overlay overlay-black overlay-40 bg-img">
-  <div class="bx--grid">
-    <div class="bx--row banner--icons" aria-hidden="true">
-      {#each icons as icon}
-        <div class="bx--col-1">
-          <div class="icon">{@html icon}</div>
-        </div>
-      {/each}
-    </div>
-
-    <div class="bx--row">
-      <div class="bx--col-lg-10 bx--col-md-8 banner--content">
-        <div class="title-container">
-          <h1 class="sr-only">Cal-Adapt</h1>
-          <p class="title h1">
-            Cal-Adapt provides a way to explore peer-reviewed data that portrays
-            how climate change might affect California at the state and local
-            level.
-          </p>
-        </div>
-
-        <p class="lead">
-          We make this data available through downloads, visualizations, and the
-          Cal-Adapt API for your research, outreach, and adaptation planning
-          needs.
-        </p>
-
-        <div class="btn-container">
-          <Button icon="{ArrowRight16}" href="/about" sapper:prefetch
-            >More about Cal-Adapt</Button
-          >
-        </div>
-      </div>
-    </div>
-  </div>
-</section> -->
 
 <section class="page-grid page-grid--home">
   <div class="content">
