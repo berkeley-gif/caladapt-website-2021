7f1ff37189ef76ed6829413469754e4ecb0ef265
Chris Henrick <chrishenrick@gmail.com>
2021-11-02 17:03:49 -0700

    Homepage uses Banner partial (wip)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,7 +19,7 @@
   import { ArrowRight16 } from "carbon-icons-svelte";
 
   import { Card, CardsContainer } from "~/components/cards";
-  import SidebarRight from "~/partials/SidebarRight.svelte";
+  import { Banner, SidebarRight } from "~/partials";
   import Sun from "static/img/icons/sun.svg";
   import Rainfall from "static/img/icons/rainfall.svg";
   import Wildfire from "static/img/icons/wildfire.svg";
@@ -109,7 +109,32 @@
 </svelte:head>
 
 <!-- Banner -->
-<section class="bleed banner overlay overlay-black overlay-40 bg-img">
+<Banner
+  titleText="Cal-Adapt provides a way to explore peer-reviewed data that 
+    portrays how climate change might affect California at the state and local
+    levels."
+  subtitleText="We make this data available through downloads, visualizations, 
+    and the Cal-Adapt API for your research, outreach, and adaptation planning
+    needs."
+  bannerImg="/img/banners/yosemite_1600x540.jpg"
+  bannerImgMobile="/img/banners/yosemite_700x800.jpg"
+  overlayOpacity="{0.5}"
+>
+  <div class="bx--row banner--icons" aria-hidden="true" slot="icons">
+    {#each icons as icon}
+      <div class="bx--col-1">
+        <div class="icon">{@html icon}</div>
+      </div>
+    {/each}
+  </div>
+  <div class="btn-container" slot="button">
+    <Button icon="{ArrowRight16}" href="/about" sapper:prefetch
+      >More about Cal-Adapt</Button
+    >
+  </div>
+</Banner>
+
+<!-- <section class="bleed banner overlay overlay-black overlay-40 bg-img">
   <div class="bx--grid">
     <div class="bx--row banner--icons" aria-hidden="true">
       {#each icons as icon}
@@ -144,7 +169,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
 
 <section class="page-grid page-grid--home">
   <div class="content">
