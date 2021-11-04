25f929bcc1cdf3e6932302873f21fcf4548cf3d2
Chris Henrick <chrishenrick@gmail.com>
2021-11-04 12:57:32 -0700

    Header tool partial uses Banner partial

--- src/components/tools/Partials/Header.svelte
+++ src/components/tools/Partials/Header.svelte
@@ -1,51 +1,16 @@
 <script>
+  import { Banner } from "~/partials";
   export let title;
   export let iconPaths;
+  export let description;
 </script>
 
-<style lang="scss">
-  .banner {
-    background: var(--gray-10);
-    color: var(--text-01);
-    padding: 3rem 0;
-
-    h1 {
-      margin-top: var(--spacing-8);
-    }
-
-    img {
-      width: 60px;
-      margin-left: 0.5rem;
-
-      &:first-of-type {
-        margin-left: 0;
-      }
-    }
-  }
-
-  .banner :global(.lead a) {
-    color: var(--link-01);
-    text-decoration: none;
-  }
-</style>
-
-<!-- Banner -->
-<div class="bleed banner">
-  <div class="bx--grid">
-    <div class="bx--row">
-      <div class="bx--col-lg-12 bx--col-md-8 bx--col-sm-4">
-        <div class="title">
-          {#if Array.isArray(iconPaths) && iconPaths.length}
-            {#each iconPaths as icon}
-              <img src="{icon}" alt="icon" class="icon" />
-            {/each}
-          {/if}
-        </div>
-        <h1>{title}</h1>
-        <slot name="description">
-          <em>[Provide a brief description for this tool]</em>
-        </slot>
-      </div>
-    </div>
-  </div>
-</div>
+<Banner
+  titleText="{title}"
+  subtitleText="{description}"
+  iconPaths="{iconPaths}"
+  useOffset="{false}"
+  bgColor="var(--gray-10)"
+  textColor="var(--text-01)"
+  overlayColor="transparent"
+/>
