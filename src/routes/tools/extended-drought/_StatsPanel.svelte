ff8b1afd5862166223873459a94ba47388bdff66
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-11-05 19:41:06 -0700

    Add new Stats component

--- src/routes/tools/extended-drought/_StatsPanel.svelte
+++ src/routes/tools/extended-drought/_StatsPanel.svelte
@@ -1,37 +1,34 @@
 <script>
-  import { RangeAvg } from "~/components/tools/Stats";
+  import { DEFAULT_STAT_GROUPS } from "../_common/constants";
+  import { DEFAULT_STAT_PERIODS } from "./_constants";
+  import { AvgRange } from "~/components/tools/Stats";
 
   export let units;
   export let data;
   export let formatFn;
+  export let models;
+  export let scenario;
 </script>
 
 <ul class="stats">
   <li class="block">
-    <RangeAvg
+    <AvgRange
       units="{units}"
-      data="{data}"
-      series="{'historical'}"
-      period="{'baseline'}"
+      data="{data ? data.filter((d) => d.date.getUTCFullYear() < 2006) : null}"
+      groupList="{DEFAULT_STAT_GROUPS.filter(({ id }) => id === 'observed')}"
+      periodList="{DEFAULT_STAT_PERIODS.filter((d) => d.historical)}"
       format="{formatFn}"
+      models="{models}"
     />
   </li>
   <li class="block">
-    <RangeAvg
+    <AvgRange
       units="{units}"
-      data="{data}"
-      series="{'future'}"
-      period="{'mid-century'}"
+      data="{data ? data.filter((d) => d.date.getUTCFullYear() >= 2006) : null}"
+      groupList="{DEFAULT_STAT_GROUPS.filter((d) => !d.historical)}"
+      periodList="{DEFAULT_STAT_PERIODS.filter(({ id }) => id === scenario)}"
       format="{formatFn}"
-    />
-  </li>
-  <li class="block">
-    <RangeAvg
-      units="{units}"
-      data="{data}"
-      series="{'future'}"
-      period="{'end-century'}"
-      format="{formatFn}"
+      models="{models}"
     />
   </li>
 </ul>
