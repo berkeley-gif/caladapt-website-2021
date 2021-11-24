7165efef5906c863c7f5e91274154db9a200765d
Chris Henrick <chrishenrick@gmail.com>
2021-11-24 14:52:29 -0800

    scroll expanded accordion item into view after update

--- src/routes/help/_ItemsAccordion.svelte
+++ src/routes/help/_ItemsAccordion.svelte
@@ -1,10 +1,9 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, afterUpdate } from "svelte";
   import { Accordion, AccordionItem } from "carbon-components-svelte";
   export let items;
 
-  let hash = "";
-
+  $: hash = "";
   $: isOpen = (title) => makeId(title) === hash;
 
   function makeId(string) {
@@ -19,6 +18,19 @@
 
   onMount(() => {
     hash = getLocationHashText();
+    window.addEventListener(
+      "hashchange",
+      () => {
+        hash = getLocationHashText();
+      },
+      false
+    );
+  });
+
+  afterUpdate(() => {
+    if (document.getElementById(`${hash}`)) {
+      document.getElementById(`${hash}`).scrollIntoView();
+    }
   });
 </script>
 
@@ -36,10 +48,7 @@
   {#each items as { metadata, html }}
     <AccordionItem open="{isOpen(metadata.title)}">
       <div slot="title">
-        <h2
-          id="{metadata.title ? makeId(metadata.title) : ''}"
-          class="bx--accordion__title"
-        >
+        <h2 id="{makeId(metadata.title)}" class="bx--accordion__title">
           {metadata.title}
         </h2>
       </div>
