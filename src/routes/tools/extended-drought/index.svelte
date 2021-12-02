7d8cea041976b0e44b6ea55fbc291ef62e7298b9
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-12-01 21:04:06 -0800

    update intro text

--- src/routes/tools/extended-drought/index.svelte
+++ src/routes/tools/extended-drought/index.svelte
@@ -218,7 +218,7 @@
 <Header
   iconPaths="{tool.icons}"
   title="{tool.title}"
-  description="{tool.desc}"
+  description="California has a highly variable climate and is susceptible to dry spells. Recent research suggests that extended drought occurrence (“mega-drought”) could become more pervasive in future decades. This tool explores data for two 20-year drought scenarios derived from LOCA downscaled meteorological and hydrological simulations."
 />
 
 <ToolNavigation href="{`/tools/${tool.slug}`}" />
