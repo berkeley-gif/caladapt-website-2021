14c4034e4f530c73e0f52cdc1b5ebcb369ef6421
Chris Henrick <chrishenrick@gmail.com>
2021-12-03 10:20:24 -0800

    add try/catch to logException

--- src/helpers/logging.js
+++ src/helpers/logging.js
@@ -1,8 +1,12 @@
 export const logException = (error, fatal = false) => {
   if (process.env.NODE_ENV === "production") {
-    gtag("event", "exception", {
-      description: typeof error === "object" ? error.message : error,
-      fatal,
-    });
+    try {
+      gtag("event", "exception", {
+        description: typeof error === "object" ? error.message : `${error}`,
+        fatal,
+      });
+    } catch (e) {
+      console.warn("gtag exception event capture failed: ", e.message);
+    }
   }
 };
