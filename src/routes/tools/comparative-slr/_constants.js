12fc6d13f02f474d6b5c8a394d890ff55a7d31bf
Chris Henrick <chrishenrick@gmail.com>
2021-12-08 17:36:42 -0800

    updated constants & stores

--- src/routes/tools/comparative-slr/_constants.js
+++ src/routes/tools/comparative-slr/_constants.js
@@ -1,7 +1,7 @@
 export const DEFAULT_CENTER = [-122.2813, 37.7813];
-export const DEFAULT_FLOOD_SCENARIO = "max";
-export const DEFAULT_TIME_FRAME = [2020, 2040];
-export const DEFAULT_DATA_LAYERS = ["CosMoS", "CalFlod3D"];
+export const DEFAULT_FLOOD_SCENARIO = "med";
+export const DEFAULT_TIME_FRAME = "[2020,2040]";
+export const DEFAULT_DATA_LAYERS = undefined;
 
 export const DEFAULT_INITIAL_CONFIG = {
   boundaryId: "locagrid",
@@ -11,3 +11,44 @@ export const DEFAULT_INITIAL_CONFIG = {
   lat: DEFAULT_CENTER[1],
   imperial: false,
 };
+
+export const TIME_PERIODS = [
+  {
+    id: "[2020,2040]",
+    label: "2020–2040",
+  },
+  {
+    id: "[2080,2100]",
+    label: "2080–2100",
+  },
+];
+
+export const FLOOD_SCENARIOS = [
+  {
+    id: "min",
+    label: "Minimum",
+  },
+  {
+    id: "med",
+    label: "Median",
+  },
+  {
+    id: "max",
+    label: "Maximum",
+  },
+];
+
+export const DATA_LAYERS = [
+  {
+    id: "calflod3dtfs_5m",
+    label: "CalFloD3D-TFS (5m)",
+  },
+  {
+    id: "calflod3dtfs_50m",
+    label: "CalFloD3D-TFS (50m)",
+  },
+  {
+    id: "cosmos",
+    label: "CoSMoS",
+  },
+];
--- src/routes/tools/comparative-slr/_store.js
+++ src/routes/tools/comparative-slr/_store.js
@@ -1,22 +1,41 @@
 import { makeCustomWritableStore } from "../_common/stores";
+import {
+  DEFAULT_FLOOD_SCENARIO,
+  DEFAULT_TIME_FRAME,
+  DEFAULT_DATA_LAYERS,
+  FLOOD_SCENARIOS,
+  TIME_PERIODS,
+  DATA_LAYERS,
+} from "./_constants";
 
-export const floodScenarioStore = makeCustomWritableStore("max", {
-  name: "floodScenarioStore",
-});
+export const floodScenarioStore = makeCustomWritableStore(
+  DEFAULT_FLOOD_SCENARIO,
+  {
+    name: "floodScenarioStore",
+    getters: [
+      {
+        name: "scenario",
+        getter: ($s) => FLOOD_SCENARIOS.find((d) => d.id === $s),
+      },
+    ],
+  }
+);
 
-export const timeFrameStore = makeCustomWritableStore([2020, 2040], {
+export const timeFrameStore = makeCustomWritableStore(DEFAULT_TIME_FRAME, {
   name: "timeFrameStore",
   getters: [
     {
-      name: "label",
-      getter: ($s) => (Array.isArray($s) ? $s.join("–") : ""),
+      name: "timeFrame",
+      getter: ($s) => TIME_PERIODS.find((d) => d.id === $s),
+    },
+    {
+      name: "parsed",
+      getter: ($s) => JSON.parse($s),
     },
   ],
 });
 
-export const dataLayersStore = makeCustomWritableStore(
-  ["CosMoS", "CalFlod3D"],
-  {
-    name: "dataLayersStore",
-  }
-);
+export const dataLayersStore = makeCustomWritableStore(DEFAULT_DATA_LAYERS, {
+  name: "layers",
+  getter: ($s) => DATA_LAYERS.find((d) => d.id === $s) || [],
+});
