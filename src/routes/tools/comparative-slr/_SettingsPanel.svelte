29c2899855fc5cbe213a1c59710fb19d32b8c1e7
Chris Henrick <chrishenrick@gmail.com>
2021-12-08 17:39:11 -0800

    added form controls in SettingsPanel

--- src/routes/tools/comparative-slr/_SettingsPanel.svelte
+++ src/routes/tools/comparative-slr/_SettingsPanel.svelte
@@ -8,11 +8,20 @@
     dataLayersStore,
   } from "./_store";
 
+  import { TIME_PERIODS, FLOOD_SCENARIOS, DATA_LAYERS } from "./_constants";
+
   import { LearnMoreButton } from "~/components/tools/Partials";
   import { StaticMap } from "~/components/tools/Location";
+  import { RadioBtnGroup, SelectLayers } from "~/components/tools/Settings";
 
   const dispatch = createEventDispatcher();
   const { location } = locationStore;
+
+  const layerOptions = DATA_LAYERS.map((d) => ({
+    ...d,
+    disabled: false,
+    checked: false,
+  }));
 
   function showLearnMore({ slugs = [], content = "", header = "Glossary" }) {
     dispatch("showLearnMore", { slugs, content, header });
@@ -55,13 +64,43 @@
 </div>
 
 <div class="block">
-  <span class="bx--label">Select Time Frame</span>
+  <RadioBtnGroup
+    title="Select Time Period"
+    items="{TIME_PERIODS}"
+    selected="{$timeFrameStore}"
+  />
+  <LearnMoreButton
+    on:click="{() =>
+      showLearnMore({
+        content: 'To do...',
+        header: 'Select Time Period',
+      })}"
+  />
 </div>
 
 <div class="block">
-  <span class="bx--label">Select Flood Scenario</span>
+  <RadioBtnGroup
+    title="Select Flood Scenario"
+    items="{FLOOD_SCENARIOS}"
+    selected="{$floodScenarioStore}"
+  />
+  <LearnMoreButton
+    on:click="{() =>
+      showLearnMore({
+        content: 'To do...',
+        header: 'Select Flood Scenario',
+      })}"
+  />
 </div>
 
 <div class="block">
   <span class="bx--label">Select Data Layers</span>
+  <SelectLayers items="{layerOptions}" />
+  <LearnMoreButton
+    on:click="{() =>
+      showLearnMore({
+        content: 'To do...',
+        header: 'Select Data Layers',
+      })}"
+  />
 </div>
