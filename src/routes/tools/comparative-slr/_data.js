0bef47d74f1ca03e4546bfd953efa302394d3af3
Chris Henrick <chrishenrick@gmail.com>
2021-12-09 16:03:53 -0800

    handle cosmos tile url separately from calflod

--- src/routes/tools/comparative-slr/_data.js
+++ src/routes/tools/comparative-slr/_data.js
@@ -6,11 +6,19 @@ const {
   },
 } = config;
 
-export const getTileUrlCalflod3d = (scenario, timeFrame, color) =>
-  `${tileURL}/calflod3dtfs_${scenario}_${timeFrame}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
-
-// "https://api.cal-adapt.org/tiles/calflod3dtfs_mosaic_med_2020-2040_sfbay/{z}/{x}/{y}.png",
-// "https://api.cal-adapt.org/tiles/calflod3dtfs_mosaic_med_2020-2040_sfbay/10/161/397.png?style=rsblue"
-
-export const getTileUrl = (source, scenario, timeFrame, region, color) =>
+export const getTileUrlCalflod3d = (
+  source,
+  scenario,
+  timeFrame,
+  region,
+  color
+) =>
   `${tileURL}/${source}_${scenario}_${timeFrame}_mosaic_${region}/{z}/{x}/{y}.png?style=${color}`;
+
+export const getTileUrlCosmos = (cm = 200, color = "rsgreen") =>
+  `https://api.cal-adapt.org/tiles/cosmosflooding_${cm}cm_100yrstorm_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
+
+export const getTileUrl = (source, ...rest) =>
+  source === "cosmos"
+    ? getTileUrlCosmos()
+    : getTileUrlCalflod3d(source, ...rest);
