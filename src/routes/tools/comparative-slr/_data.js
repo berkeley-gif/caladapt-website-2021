34bf9da907db89f03edcb7fed6420be61d7264b2
Chris Henrick <chrishenrick@gmail.com>
2021-12-10 16:33:51 -0800

    handle tile urls for calflod 50m data

--- src/routes/tools/comparative-slr/_data.js
+++ src/routes/tools/comparative-slr/_data.js
@@ -1,5 +1,5 @@
 import config from "~/helpers/api-config";
-import { cosmosLookup } from "./_cosmos-look-up";
+import { cosmosLookup, calflod50mLookup } from "./_data-layers-lookup";
 
 const {
   env: {
@@ -7,26 +7,38 @@ const {
   },
 } = config;
 
-export const getTileUrlCalflod3d = (
-  source,
-  scenario,
-  timeFrame,
-  region,
-  color
-) =>
+const getTileUrlCalflod3d = (source, scenario, timeFrame, region, color) =>
   `${tileURL}/${source}_${scenario}_${timeFrame}_mosaic_${region}/{z}/{x}/{y}.png?style=${color}`;
 
-export const getTileUrlCosmos = (timeFrame, scenario, color = "rsgreen") => {
+const getTileUrlCalflod3d50m = (timeFrame, scenario, color = "rsblue") => {
+  const id = `${timeFrame}|${scenario}`;
+  const slug = calflod50mLookup.get(id);
+  if (slug) {
+    return `${tileURL}/${slug}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
+  } else {
+    console.error(`calflod3d-tfs-50m tile slug not found for ${id}`);
+  }
+};
+
+const getTileUrlCosmos = (timeFrame, scenario, color = "rsgreen") => {
   const id = `${timeFrame}|${scenario}`;
   const slug = cosmosLookup.get(id);
   if (slug) {
-    return `https://api.cal-adapt.org/tiles/${slug}/{z}/{x}/{y}.png?style=${color}`;
+    return `https://api.cal-adapt.org/tiles/${slug}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
   } else {
     console.error(`cosmos tile slug not found for ${id}`);
   }
 };
 
-export const getTileUrl = (source, scenario, timeFrame, ...rest) =>
-  source === "cosmos"
-    ? getTileUrlCosmos(timeFrame, scenario)
-    : getTileUrlCalflod3d(source, scenario, timeFrame, ...rest);
+export const getTileUrl = (source, scenario, timeFrame, ...rest) => {
+  switch (source) {
+    case "cosmos":
+      return getTileUrlCosmos(timeFrame, scenario);
+    case "calflod3dtfs":
+      return getTileUrlCalflod3d(source, scenario, timeFrame, ...rest);
+    case "calflod3dtfs_50m":
+      return getTileUrlCalflod3d50m(timeFrame, scenario);
+    default:
+      throw new Error("unrecognized slr data source");
+  }
+};
