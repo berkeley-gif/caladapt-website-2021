b8f8d0460d6010a32783d6fc6fed1bd56a7c3c2d
Chris Henrick <chrishenrick@gmail.com>
2021-12-10 16:18:54 -0800

    added calflod 50m lookup

--- src/routes/tools/comparative-slr/_cosmos-look-up.js
+++ src/routes/tools/comparative-slr/_cosmos-look-up.js
@@ -6,3 +6,22 @@ export const cosmosLookup = new Map([
   ["2080-2100|med", "cosmosflooding_150cm_100yrstorm_mosaic_sfbay"],
   ["2080-2100|max", "cosmosflooding_200cm_100yrstorm_mosaic_sfbay"],
 ]);
+
+export const calflod50mLookup = new Map([
+  [
+    "2020-2040|min",
+    "calflod3dtfs_50m_HadGEM2-ES_rcp45_2020-2040_prec500_mosaic_sfbay",
+  ],
+  [
+    "2020-2040|max",
+    "calflod3dtfs_50m_CanESM2_rcp45_2020-2040_prec999_mosaic_sfbay",
+  ],
+  [
+    "2080-2100|min",
+    "calflod3dtfs_50m_HadGEM2-ES_rcp85_2080-2100_prec500_mosaic_sfbay",
+  ],
+  [
+    "2080-2100|max",
+    "calflod3dtfs_50m_CanESM2_rcp85_2080-2100_prec999_mosaic_sfbay",
+  ],
+]);
