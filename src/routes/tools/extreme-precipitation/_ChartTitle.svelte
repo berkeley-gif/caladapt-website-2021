26a34bc8f9bcedcee483a54d0c6b9c729642f00c
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-12-20 11:28:23 +0530

    update chart title for intensity chart

--- src/routes/tools/extreme-precipitation/_ChartTitle.svelte
+++ src/routes/tools/extreme-precipitation/_ChartTitle.svelte
@@ -4,19 +4,11 @@
 
   export let title;
   export let indicatorLabel;
-  export let climvarLabel;
+  export let eventLabel;
   export let scenarioLabel;
   export let thresholdLabel;
   export let loadLocation;
-  export let durationLabel;
-
-  let label;
-
-  $: if (indicatorLabel.includes("Heat Wave")) {
-    label = indicatorLabel.replace("Number of", `Number of ${durationLabel}`);
-  } else {
-    label = indicatorLabel;
-  }
+  export let intervalsLabel;
 </script>
 
 <style>
@@ -34,12 +26,32 @@
 >
   Change Location
 </Button>
+
 <h2 class="h3">
   {title}
 </h2>
-<p class="h4">
-  Projected changes in <span class="annotate">{label}</span> when
-  <span class="annotate">{climvarLabel}</span>
-  is above <span class="annotate">{thresholdLabel}</span> under a
-  <span class="annotate">{scenarioLabel}</span>.
-</p>
+
+{#if indicatorLabel.includes("Intensity")}
+  <p class="h4">
+    Projected changes in <span class="annotate">{indicatorLabel}</span> which
+    are are exceeded on average once every
+    <span class="annotate">{intervalsLabel}</span>
+    under a
+    <span class="annotate">{scenarioLabel}</span>.
+  </p>
+  <p>
+    Extreme Precipitation events are consecutive days in which the <span
+      class="annotate">{eventLabel}</span
+    >
+    is above an extreme threshold of
+    <span class="annotate">{thresholdLabel}</span>.
+  </p>
+{:else}
+  <p class="h4">
+    Projected changes in <span class="annotate">{indicatorLabel}</span> with
+    <span class="annotate">{eventLabel}</span>
+    above an extreme threshold of <span class="annotate">{thresholdLabel}</span>
+    under a
+    <span class="annotate">{scenarioLabel}</span>.
+  </p>
+{/if}
