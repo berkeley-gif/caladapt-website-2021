38749b33146049340a66612c0409ea8395c77e21
Chris Henrick <chrishenrick@gmail.com>
2021-12-22 14:47:55 -0800

    use new cosmos slug for xyz map tiles

--- src/routes/tools/slr-coastal-inundation/_data.js
+++ src/routes/tools/slr-coastal-inundation/_data.js
@@ -1,5 +1,5 @@
 import config from "~/helpers/api-config";
-import { cosmosLookup, calflod50mLookup } from "./_data-layers-lookup";
+import { calflod50mLookup } from "./_data-layers-lookup";
 
 const {
   env: {
@@ -21,16 +21,9 @@ const getTileUrlCalflod3d50m = (timeFrame, scenario, color = "rsblue") => {
   }
 };
 
-const getTileUrlCosmos = (timeFrame, scenario, color = "rsgreen") => {
-  const id = `${timeFrame}|${scenario}`;
-  const slug = cosmosLookup.get(id);
-  if (slug) {
-    return `https://api.cal-adapt.org/tiles/${slug}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
-  } else {
-    console.error(`cosmos tile slug not found for ${id}`);
-    return "";
-  }
-};
+// https://api.cal-adapt.org/tiles/cosmosflooding_2080-2100_max_mosaic_sfbay/{z}/{x}/{y}.png
+const getTileUrlCosmos = (timeFrame, scenario, color = "rsgreen") =>
+  `${tileURL}/cosmosflooding_${timeFrame}_${scenario}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
 
 export const getTileUrl = (source, scenario, timeFrame, ...rest) => {
   switch (source) {
