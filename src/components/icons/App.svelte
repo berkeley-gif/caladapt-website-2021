8c97c84274795025eeb80d24c5d57174952a4a21
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-12-22 08:44:58 +0530

    fix a11y for icons

--- src/components/icons/App.svelte
+++ src/components/icons/App.svelte
@@ -1,43 +1,26 @@
 <script>
-  let className = undefined;
-  export { className as class };
-  export let id = undefined;
-  export let tabindex = undefined;
-  export let focusable = false;
   export let title = undefined;
   export let style = undefined;
   export let dimension = 60;
 
-  $: ariaLabel = $$props["aria-label"];
-  $: ariaLabelledBy = $$props["aria-labelledby"];
-  $: labelled = ariaLabel || ariaLabelledBy || title;
+  let id = "icon-title-app";
   $: attributes = {
+    id,
     width: dimension,
     height: dimension,
-    role: labelled ? "img" : undefined,
-    focusable: tabindex === "0" ? true : focusable,
-    tabindex,
+    role: "img",
+    "aria-labelledby": id,
+    ...(style && { style }),
   };
 </script>
 
 <svg
-  on:click
-  on:mouseover
-  on:focus
-  on:mouseenter
-  on:mouseleave
-  on:keyup
-  on:keydown
   xmlns="http://www.w3.org/2000/svg"
-  class="{className}"
   preserveAspectRatio="xMidYMid meet"
-  style="{style}"
-  id="{id}"
   viewBox="0 0 32 32"
-  aria-labelledby="iconTitle"
   {...attributes}
 >
-  <title id="iconTitle">All Apps</title>
+  <title id="{id}">{title ? title : "Icon"}</title>
   <path
     fill="#fff"
     d="M8,4V8H4V4Zm2-2H2v8h8Zm8,2V8H14V4Zm2-2H12v8h8Zm8,2V8H24V4Zm2-2H22v8h8ZM8,14v4H4V14Zm2-2H2v8h8Zm8,2v4H14V14Zm2-2H12v8h8Zm8,2v4H24V14Zm2-2H22v8h8ZM8,24v4H4V24Zm2-2H2v8h8Zm8,2v4H14V24Zm2-2H12v8h8Zm8,2v4H24V24Zm2-2H22v8h8Z"
--- src/components/icons/Wildfire.svelte
+++ src/components/icons/Wildfire.svelte
@@ -1,43 +1,26 @@
 <script>
-  let className = undefined;
-  export { className as class };
-  export let id = undefined;
-  export let tabindex = undefined;
-  export let focusable = false;
   export let title = undefined;
   export let style = undefined;
   export let dimension = 60;
 
-  $: ariaLabel = $$props["aria-label"];
-  $: ariaLabelledBy = $$props["aria-labelledby"];
-  $: labelled = ariaLabel || ariaLabelledBy || title;
+  let id = "icon-title-wildfire";
   $: attributes = {
+    id,
     width: dimension,
     height: dimension,
-    role: labelled ? "img" : undefined,
-    focusable: tabindex === "0" ? true : focusable,
-    tabindex,
+    role: "img",
+    "aria-labelledby": id,
+    ...(style && { style }),
   };
 </script>
 
 <svg
-  on:click
-  on:mouseover
-  on:focus
-  on:mouseenter
-  on:mouseleave
-  on:keyup
-  on:keydown
   xmlns="http://www.w3.org/2000/svg"
-  class="{className}"
   preserveAspectRatio="xMidYMid meet"
-  style="{style}"
-  id="{id}"
   viewBox="0 0 62 62"
-  aria-labelledby="iconTitle"
   {...attributes}
 >
-  <title id="iconTitle">Wildfire Icon</title>
+  <title id="{id}">{title ? title : "Icon"}</title>
   <defs>
     <radialGradient id="gradient-wildfire">
       <stop offset="0" style="stop-color:#C8C8C8;stop-opacity:0.3"></stop>
