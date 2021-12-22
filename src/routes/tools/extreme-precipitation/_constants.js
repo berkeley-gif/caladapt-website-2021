100fde3dfce9fbff31cc7643e66192cd7b2406e7
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2021-12-22 18:26:48 +0530

    update intensity chart description

--- src/routes/tools/extreme-precipitation/_constants.js
+++ src/routes/tools/extreme-precipitation/_constants.js
@@ -18,10 +18,10 @@ export const CLIMATE_INDICATORS = [
     decimals: 0,
     chartComponent: DotAndWhiskerChart,
     statsComponent: AvgRange,
-    description: `<p>The colored lines on this visualization represent
-    a timeseries of number of extreme precipitation events from individual downscaled GCMs.
-    The historical observed data is represented by
-    a gray line from 1950-2006.</p><p>Click on any of the legend keys to highlight
+    description: `<p>The colored dots on this visualization represent
+    the estimated intensity (return levels) of extreme precipitation events from individual downscaled GCMs.
+    The historical observed data is represented by a gray dot. The width of the whiskers (light gray lines) span 
+    the 95% confidence interval.</p><p>Click on any of the legend keys to highlight
     corresponding timeseries.</p>`,
   },
   {
@@ -93,6 +93,7 @@ export const DEFAULT_CLIMATE_INDICATOR = "intensity";
 export const DEFAULT_DURATION = 2;
 export const DEFAULT_THRESHOLD_TYPE = "max";
 export const DEFAULT_RETURN_PERIOD = 20;
+export const DEFAULT_ROLLING_FUNCTION = "sum";
 
 export const HEATMAP_COLOR_SCALE = ["#19cdbb", "#368ce1", "#6d41ab"];
 
@@ -100,6 +101,7 @@ export const INDICATOR_DESCRIPTION = `
 <p>Indicators presented in this tool examine projected trends in four key characteristics of 
 Extreme Precipitation Events:</p>
 <ul>
+  <li><strong>Intensity</strong>: The number of Extreme Precipitation Events that occur every year.</li>
   <li><strong>Frequency</strong>: The number of Extreme Precipitation Events that occur every year.</li>
   <li><strong>Timing</strong>: The length of the season between the first Extreme Heat Day/Warm 
   Night and the last.</li>
