32b2a43cd96201fc2d5ffe8dc9394166d01d0eb3
Chris Henrick <chrishenrick@gmail.com>
2022-01-05 15:56:37 -0800

    rm unused tile layer url fns

--- src/routes/tools/slr-coastal-inundation/_data-layers-lookup.js
+++
@@ -1,7 +0,0 @@
-// DEPRECIATED: tile URLs are now sourced from raster tile metadata returned by the API
-export const calflod50mLookup = new Map([
-  ["2020-2040|min", "calflod3dtfs_50m_HadGEM2-ES_rcp45_2020-2040_prec500"],
-  ["2020-2040|max", "calflod3dtfs_50m_CanESM2_rcp45_2020-2040_prec999"],
-  ["2080-2100|min", "calflod3dtfs_50m_HadGEM2-ES_rcp85_2080-2100_prec500"],
-  ["2080-2100|max", "calflod3dtfs_50m_CanESM2_rcp85_2080-2100_prec999"],
-]);
--- src/routes/tools/slr-coastal-inundation/_data.js
+++ src/routes/tools/slr-coastal-inundation/_data.js
@@ -1,11 +1,10 @@
 import bboxPolygon from "@turf/bbox-polygon";
 import config from "~/helpers/api-config";
 import { logException } from "~/helpers/logging";
-import { calflod50mLookup } from "./_data-layers-lookup";
 
 const {
   env: {
-    production: { tileURL, apiEndpoint },
+    production: { apiEndpoint },
   },
 } = config;
 
@@ -13,7 +12,7 @@ export const getRasterMetaData = (scenario, source, timeFrame, geom) =>
   fetch(
     `${apiEndpoint}/rstores/?slug=${source}&slug=${scenario}&slug=${timeFrame}&bbintersects=${encodeURIComponent(
       geom
-    )}&cachebust=foo`
+    )}`
   ).then((res) => res.json());
 
 export const toBBoxPolygon = (coords) => {
@@ -24,39 +23,5 @@ export const toBBoxPolygon = (coords) => {
       console.error(error);
       logException(error);
     }
-  }
-};
-
-// DEPRECIATED: tile URLs are now sourced from raster tile metadata returned by the API
-const getTileUrlCalflod3d = (source, scenario, timeFrame, region, color) =>
-  `${tileURL}/${source}_${scenario}_${timeFrame}_mosaic_${region}/{z}/{x}/{y}.png?style=${color}`;
-
-// DEPRECIATED: tile URLs are now sourced from raster tile metadata returned by the API
-const getTileUrlCalflod3d50m = (timeFrame, scenario, color = "rsblue") => {
-  const id = `${timeFrame}|${scenario}`;
-  const slug = calflod50mLookup.get(id);
-  if (slug) {
-    return `${tileURL}/${slug}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
-  } else {
-    console.error(`calflod3d-tfs-50m tile slug not found for ${id}`);
-    return "";
-  }
-};
-
-// DEPRECIATED: tile URLs are now sourced from raster tile metadata returned by the API
-const getTileUrlCosmos = (timeFrame, scenario, color = "rsgreen") =>
-  `${tileURL}/cosmosflooding_${timeFrame}_${scenario}_mosaic_sfbay/{z}/{x}/{y}.png?style=${color}`;
-
-// DEPRECIATED: tile URLs are now sourced from raster tile metadata returned by the API
-export const getTileUrl = (source, scenario, timeFrame, ...rest) => {
-  switch (source) {
-    case "cosmos":
-      return getTileUrlCosmos(timeFrame, scenario);
-    case "calflod3dtfs":
-      return getTileUrlCalflod3d(source, scenario, timeFrame, ...rest);
-    case "calflod3dtfs_50m":
-      return getTileUrlCalflod3d50m(timeFrame, scenario);
-    default:
-      throw new Error("unrecognized slr data source");
   }
 };
