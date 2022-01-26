a730386b8b0b0a0868c7d7fc9157951573b369ae
Chris Henrick <chrishenrick@gmail.com>
2022-01-26 14:53:27 -0800

    updated content/resources/data to include new slr entries

--- content/resources/data.js
+++ content/resources/data.js
@@ -2,24 +2,72 @@ const data = [
   {
     date: '2020-01-20',
     title: 'California Adaptation Clearinghouse',
-    text: `The Adaptation Clearinghouse is the State of California’s consolidated searchable 
+    text: `The Adaptation Clearinghouse is the State of California's consolidated searchable 
     database of resources for local, regional and statewide climate adaptation planning 
     and decision-making.`,
-    category: 'climate',
     link: 'https://resilientca.org/',
     image: '/logos/clearinghouse.jpg',
-    author: 'Governor’s Office of Planning and Research ',
+    author: "Governor's Office of Planning and Research",
   },
   {
     date: '2020-01-20',
     title: "California's 4th Climate Change Assessment",
-    text: `California’s Fourth Climate Change Assessment advances actionable 
+    text: `California's Fourth Climate Change Assessment advances actionable 
     science that serves the growing needs of state and local-level decision-makers from a 
     variety of sectors.`,
-    category: 'climate',
     link: 'https://www.climateassessment.ca.gov/',
     image: '/logos/ccc4a.jpg',
     author: 'State',
+  },
+  {
+    title: "Rising Seas in California",
+    text: `Peer reviewed synthesis of current state of sea-level rise science 
+      from California Ocean Protection Council used by several state agencies 
+      in developing guidance for planning decisions.`,
+    link: "http://www.opc.ca.gov/webmaster/ftp/pdf/docs/rising-seas-in-california-an-update-on-sea-level-rise-science.pdf",
+    image: "",
+    author: "State"
+  },
+  {
+    title: "State of California Sea Level Rise Guidance",
+    text: `Latest guidance from California Ocean Protection Council for incorporating 
+      sea-level rise in risk assessment and planning based on the Rising Seas report.`,
+    link: "https://www.opc.ca.gov/webmaster/ftp/pdf/agenda_items/20180314/Item3_Exhibit-A_OPC_SLR_Guidance-rd3.pdf",
+    image: "",
+    author: "State"
+  },
+  {
+    title: "California Coastal Commission Sea Level Rise Policy Guidance",
+    text: `Updated planning guidance from California Coastal Commission for 
+      addressing sea level rise in Local Coastal Programs and Coastal Development Permits.`,
+    link: "https://www.coastal.ca.gov/climate/slrguidance.html",
+    image: "",
+    author: "State"
+  },
+  {
+    title: "Sea the Future",
+    text: `Explore and understand features of the multiple sea level rise and 
+      flood visualization tools available in California.`,
+    link: "https://www.seathefuture.org/#/",
+    image: "",
+    author: ""
+  },
+  {
+    title: "Our Coast Our Future",
+    text: `A decision support tool using CoSMoS data that provides coastal 
+      decision makers with locally relevant data to help understand, visualize, 
+      and adapt to sea level rise and coastal storms.`,
+    link: "https://ourcoastourfuture.org/",
+    image: "",
+    author: ""
+  },
+  {
+    title: "Hazard Exposure and Reporting Analytics",
+    text: `Data and tools from USGS to map and estimate community exposure to 
+      various natural hazards.`,
+    link: "https://www.usgs.gov/apps/hera/index.php",
+    image: "",
+    author: "USGS"
   },
 ];
 
