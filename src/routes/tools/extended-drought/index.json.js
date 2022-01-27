0d16f11ff5ed7ced06714b27588fce321adb96df
Chris Henrick <chrishenrick@gmail.com>
2022-01-26 17:22:13 -0800

    extended drought uses markdown for about section

---
+++ src/routes/tools/extended-drought/index.json.js
@@ -0,0 +1,25 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import marked from "marked";
+
+export function get(_req, res) {
+  try {
+    const file = "about.md";
+    const dir = "content/tools/extended-drought/";
+    const path = resolve(process.cwd(), dir, file);
+    const data = readFileSync(path, "utf-8");
+    const aboutContent = marked(data);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ aboutContent }));
+  } catch (error) {
+    console.error(error);
+    res.writeHead(404, {
+      "Content-Type": "application/json",
+    });
+    res.end(
+      JSON.stringify({
+        message: `Not found`,
+      })
+    );
+  }
+}
--- src/routes/tools/extended-drought/index.svelte
+++ src/routes/tools/extended-drought/index.svelte
@@ -30,11 +30,16 @@
       ["get-started", "faqs"].includes(d.slug)
     );
 
+    const { aboutContent } = await (
+      await this.fetch("tools/extended-drought.json")
+    ).json();
+
     return {
       tool,
       relatedTools,
       externalResources,
       helpItems,
+      aboutContent,
     };
   }
 </script>
@@ -81,6 +86,7 @@
   export let relatedTools;
   export let externalResources;
   export let helpItems;
+  export let aboutContent;
 
   const { page } = sapperStores();
   const { location, boundary } = locationStore;
@@ -220,37 +226,7 @@
       on:datasetLoaded="{(e) => datasetStore.set(e.detail)}"
     >
       <div slot="description">
-        <p>
-          Climate simulations and scenarios produced for <a
-            href="http://www.climateassessment.ca.gov/"
-            target="_blank">California's Fourth Climate Change Assessment</a
-          >
-          enable investigation of extreme, highly damaging climate changes that are
-          possible but unlikely. One of these
-          <em>low probability, high consequence events</em> is extreme drought.
-        </p>
-        <p>
-          To explore extreme drought in a warmer future, two 20-year drought
-          scenarios were produced from the LOCA downscaled meteorological and
-          hydrological simulations (<a
-            href="https://www.energy.ca.gov/sites/default/files/2019-11/Projections_CCCA4-CEC-2018-006_ADA.pdf"
-            target="_blank">Pierce et al, 2018</a
-          >). The first scenario covers the later part of this century from
-          2051-2070 and was derived from LOCA downscaled HadGEM2-ES RCP 8.5
-          simulation. The second scenario covers the earlier part of this
-          century from 2023-2042. The precipitation during this scenario is
-          daily LOCA downscaled precipitation in the sequence that it occurred
-          from 2051-2070. However, the maximum and minimum temperatures are an
-          adjusted version of 2051-2070 scenario that takes into account climate
-          warming over the century.
-        </p>
-        <p>
-          A more detailed description about the drought scenarios is available
-          in the technical Report on <a
-            href="https://www.energy.ca.gov/sites/default/files/2019-11/Projections_CCCA4-CEC-2018-006_ADA.pdf"
-            target="_blank">Climate, Drought, and Sea Level Rise Scenarios</a
-          > prepared for California's Fourth Climate Change Assessment.
-        </p>
+        {@html aboutContent}
       </div>
     </About>
   </div>
