965c9b7b75800514df73290f5ce532e323efa9f2
Chris Henrick <chrishenrick@gmail.com>
2022-01-27 10:39:42 -0800

    added description to extreme-weather content

---
+++ content/tools/extreme-weather/description.js
@@ -0,0 +1,12 @@
+export default [
+  `Explore extreme temperatures and wind speed for past weather and present day. This tool
+    provides data from 38 weather stations across California, utilizing a
+    quality-controlled dataset for hourly weather observations curated for use
+    by the energy sector (<a
+      href="https://www.energy.ca.gov/sites/default/files/2021-05/CEC-500-2020-039.pdf"
+      target="_blank">Doherty 2020</a
+    >). Present day weather conditions are from NOAA.`,
+  `<em style="font-size:1rem;">Note: This tool is still under development. We are soliciting feedback
+    from our users. Please email us at support@cal-adapt.org with your
+    comments and questions.</em>`,
+]
