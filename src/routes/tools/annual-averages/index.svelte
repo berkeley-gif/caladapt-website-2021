dca28268a68ed291c74893d8be593461978bc553
Chris Henrick <chrishenrick@gmail.com>
2022-01-26 17:17:58 -0800

    annual-averages renders about content from markdown

--- src/routes/tools/annual-averages/index.svelte
+++ src/routes/tools/annual-averages/index.svelte
@@ -33,11 +33,16 @@
       ["get-started", "faqs"].includes(d.slug)
     );
 
+    const { aboutContent } = await (
+      await this.fetch("tools/annual-averages.json")
+    ).json();
+
     return {
       tool,
       relatedTools,
       externalResources,
       helpItems,
+      aboutContent,
     };
   }
 </script>
@@ -81,6 +86,7 @@
   export let relatedTools;
   export let externalResources;
   export let helpItems;
+  export let aboutContent;
 
   // Derived stores
   const { page } = sapperStores();
@@ -205,36 +211,7 @@
       on:datasetLoaded="{(e) => datasetStore.set(e.detail)}"
     >
       <div slot="description">
-        <p>
-          Overall temperatures are projected to rise substantially throughout
-          this century. These projections differ depending on the time of year
-          and the type of measurement (highs vs. lows), all of which have
-          different potential effects to the state's ecosystem health,
-          agricultural production, water use and availability, and energy
-          demand. On average, the projections show little change in total annual
-          precipitation in California. Furthermore, among several models,
-          precipitation projections do not show a consistent trend during the
-          next century. The Mediterranean seasonal precipitation pattern is
-          expected to continue, with most precipitation falling during winter
-          from North Pacific storms. However, even modest changes would have a
-          significant impact because California ecosystems are conditioned to
-          historical precipitation levels and water resources are nearly fully
-          utilized.
-        </p>
-        <p>
-          With this tool you can explore projections of annually averaged
-          maximum temperature, minimum temperature and precipitation. These
-          climate projections have been downscaled from global climate models
-          from the <a href="https://pcmdi.llnl.gov/mips/cmip5/" target="_blank"
-            >CMIP5</a
-          >
-          archive, using the
-          <a href="http://loca.ucsd.edu/what-is-loca/" target="_blank"
-            >Localized Constructed Analogs</a
-          > (LOCA) statistical technique developed by Scripps Institution Of Oceanography.
-          LOCA is a statistical downscaling technique that uses past history to add
-          improved fine-scale detail to global climate models.
-        </p>
+        {@html aboutContent}
       </div>
     </About>
   </div>
