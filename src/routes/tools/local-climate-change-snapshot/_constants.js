272407c68e8ca298253ab8c8db50b5277215fdb6
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2022-02-09 16:07:43 -0800

    add constants used in the tool

--- src/routes/tools/local-climate-change-snapshot/_constants.js
+++ src/routes/tools/local-climate-change-snapshot/_constants.js
@@ -1,1 +1,54 @@
 export const TOOL_SLUG = "local-climate-change-snapshot";
+
+/** Regex can be used to search the Cal-Adapt API to get a list of slugs using
+ * Replace the text "indicator" in SLUG_EXP with indicator id before searching.
+ * https://api.cal-adapt.org/api/series/?slug_re=^tasmax_(year|month).*ens32
+ * https://api.cal-adapt.org/api/series/?slug_re=^tasmax_30y.*ens32
+ **/
+export const DEFAULT_TIMESERIES_SLUG_EXP = "^indicator_(year|month).*ens32";
+export const DEFAULT_SNAPSHOT_SLUG_EXP = "^indicator_30y.*ens32";
+
+/** Chart and Table data for the Area Burned indicator is assembled from the 4 GCMs.
+ * There is no ensemble equivalent in the API.
+ **/
+export const AREABURNED_TIMESERIES_SLUG_EXP = "^fire_year.*bau";
+
+export const SCENARIO_COLORS = new Map([
+  ["rsgreen", "#5DFA25"],
+  ["rsblue", "#25C7FA"],
+]);
+
+export const SERIES = [
+  {
+    id: "observed",
+    label: "Observed",
+    color: "rgba(110, 110, 110, 1)",
+    org: "University of Colorado Boulder",
+  },
+  {
+    id: "rcp45",
+    label: "Medium Emissions (RCP 4.5)",
+    color: "rgba(59, 153, 167, 1)",
+    org: "Geospatial Innovation Facility",
+  },
+  {
+    id: "rcp85",
+    label: "High Emissions (RCP 8.5)",
+    color: "rgba(131, 87, 170, 1)",
+    org: "Geospatial Innovation Facility",
+  },
+  {
+    id: "historical",
+    label: "Modeled Historical",
+    color: "rgba(170, 144, 60, 1)",
+    org: "Geospatial Innovation Facility",
+  },
+];
+
+/** Chart and Table data for the Area Burned indicator is assembled from the 4 GCMs.
+ * There is no ensemble equivalent in the API.
+ **/
+export const DEFAULT_CLIMATE_VARIABLE = "temperature";
+export const DEFAULT_CLIMATE_INDICATOR = "tasmax";
+export const DEFAULT_POLYGON_AGGREGATE_FUNCTION = "mean";
+export const AREABURNED_POLYGON_AGGREGATE_FUNCTION = "sum";
