f96171842ac924c25730662aa3dddb5bd39a4d4a
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2022-02-10 10:01:23 -0800

    add component stubs

---
+++ src/routes/tools/local-climate-change-snapshot/_ExploreData.svelte
@@ -0,0 +1,21 @@
+<script>
+  // Components
+  import LocationTitle from "./_LocationTitle.svelte";
+  import SelectCategory from "./_SelectCategory.svelte";
+  import SelectIndicator from "./_SelectIndicator.svelte";
+  import Chart from "./_DataChart.svelte";
+  import Snapshot from "./_DataSnapshot.svelte";
+  import DataNotes from "./_DataNotes.svelte";
+  import CategoryResources from "./_CategoryResources.svelte";
+
+  export let categories;
+  export let indicators;
+</script>
+
+<LocationTitle />
+<SelectCategory categories="{categories}" />
+<SelectIndicator indicators="{indicators}" />
+<Chart />
+<Snapshot />
+<DataNotes />
+<CategoryResources />
---
+++ src/routes/tools/local-climate-change-snapshot/_SelectCategory.svelte
@@ -0,0 +1,5 @@
+<script>
+  export let categories;
+
+  console.log("categories", categories.length);
+</script>
