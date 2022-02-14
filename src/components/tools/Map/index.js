3be890ec04d5f1b503a6dea1e90aa934ac2352c8
Chris Henrick <161748+clhenrick@users.noreply.github.com>
2022-02-14 15:43:45 -0800

    map layers handler util (#192)
    * add MapLayerHandler class as a util module in components/tools/Map
    
    * slr-cis tool uses MapLayerHandler from components/tools/Map
    
    * rm unused Map/RasterLayer

--- src/components/tools/Map/index.js
+++ src/components/tools/Map/index.js
@@ -6,7 +6,6 @@ export { default as Popup } from "./Popup.svelte";
 export { default as LayerToggle } from "./LayerToggle.svelte";
 export { default as BoundaryVectorLayer } from "./BoundaryVectorLayer.svelte";
 export { default as GeoJsonLayer } from "./GeoJsonLayer.svelte";
-export { default as RasterLayer } from "./RasterLayer.svelte";
 export { default as VectorLayer } from "./VectorLayer.svelte";
 export { default as BoundarySelection } from "./BoundarySelection.svelte";
 export { default as LayerInfo } from "./LayerInfo.svelte";
@@ -20,3 +19,6 @@ export { default as GeolocateControl } from "./controls/GeolocateControl.svelte"
 export { default as NavigationControl } from "./controls/NavigationControl.svelte";
 export { default as ScalingControl } from "./controls/ScalingControl.svelte";
 export { default as StyleControl } from "./controls/StyleControl.svelte";
+
+// Utilities
+export { MapLayerHandler } from "./MapLayerHandler";
