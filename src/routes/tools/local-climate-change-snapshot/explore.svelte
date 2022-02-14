5fcc6334c4741823735420e7fbede852dac27645
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2022-02-13 21:58:57 -0800

    add generic init and update functions

--- src/routes/tools/local-climate-change-snapshot/explore.svelte
+++ src/routes/tools/local-climate-change-snapshot/explore.svelte
@@ -25,16 +25,99 @@
 </script>
 
 <script>
+  import { onMount } from "svelte";
   import { Loading } from "carbon-components-svelte";
+  import { stores as sapperStores } from "@sapper/app";
+
+  // Helpers
+  import { logException } from "~/helpers/logging";
+  import { getInitialConfig, setInitialLocation } from "../_common/helpers";
+  import { INITIAL_CONFIG } from "../_common/constants";
 
   // Components
   import ExploreData from "./_ExploreData.svelte";
+  import { NotificationDisplay, notifier } from "~/components/notifications";
+
+  // Stores & constants
+  import {
+    unitsStore,
+    locationStore,
+    isFetchingStore,
+  } from "../_common/stores";
+  import {
+    categoryListStore,
+    indicatorListStore,
+    indicatorStore,
+  } from "./_store";
+  import { DEFAULT_INITIAL_CONFIG } from "./_constants";
 
   export let tool;
   export let categories;
   export let indicators;
 
-  let appReady = true;
+  // Derived stores
+  const { page } = sapperStores();
+  const { location, boundary } = locationStore;
+
+  let appReady = false;
+  let debug = process.env.NODE_ENV !== "production";
+
+  $: if (debug) {
+    console.groupCollapsed("STORE UPDATES");
+    console.table($locationStore);
+    console.table($indicatorStore);
+    console.groupEnd();
+  }
+
+  async function update() {
+    if (!appReady) return;
+    try {
+      isFetchingStore.set(true);
+    } catch (error) {
+      console.error("updateData", error);
+      logException(error);
+      notifier.error("Error", error, 2000);
+    } finally {
+      isFetchingStore.set(false);
+    }
+  }
+
+  async function initApp() {
+    const { query } = $page;
+    // Get initial configuration (from default or from url)
+    const { lat, lng, boundary, indicator, imperial } = getInitialConfig(
+      query,
+      DEFAULT_INITIAL_CONFIG
+    );
+    // Set intial values for stores
+    categoryListStore.set(categories);
+    indicatorListStore.set(indicators);
+    indicatorStore.set(indicators.find((d) => d.id === indicator));
+    unitsStore.set({ imperial });
+    const loc = await setInitialLocation(+lng, +lat, boundary);
+    locationStore.updateLocation(loc);
+    locationStore.updateBoundary(boundary);
+    return;
+  }
+
+  onMount(async () => {
+    try {
+      await initApp();
+      appReady = true;
+      if (debug) console.log("app ready");
+      await update();
+    } catch (error) {
+      console.error("init error", error);
+      logException(error);
+      notifier.error(
+        "Unable to Load Tool",
+        "Sorry! Something's probably wrong at our end. Try refereshing your browser. If you still see an error please contact us at support@cal-adapt.org.",
+        2000
+      );
+    } finally {
+      window.scrollTo(0, 0);
+    }
+  });
 </script>
 
 <svelte:head>
@@ -43,8 +126,12 @@
 
 <div id="explore-data">
   {#if appReady}
-    <ExploreData categories="{categories}" indicators="{indicators}" />
+    <ExploreData />
   {:else}
     <Loading />
   {/if}
 </div>
+
+<div class="spacing--v-96"></div>
+
+<NotificationDisplay />
