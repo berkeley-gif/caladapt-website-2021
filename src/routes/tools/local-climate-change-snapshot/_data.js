6716343803ddbc3f63aa62e10d1e149e03293ff7
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2022-02-14 17:26:18 -0800

    get data from events endpoint

--- src/routes/tools/local-climate-change-snapshot/_data.js
+++ src/routes/tools/local-climate-change-snapshot/_data.js
@@ -1,12 +1,14 @@
 // Helpers
 import config from "~/helpers/api-config";
-import { handleXHR, fetchData } from "~/helpers/utilities";
+import { handleXHR, fetchData, transformResponse } from "~/helpers/utilities";
 
 // Constants
 import {
   DEFAULT_PROJECTIONS_SLUG_EXP,
   DEFAULT_OBSERVED_SLUG_EXP,
   DEFAULT_POLYGON_AGGREGATE_FUNCTION,
+  ENVELOPE_SEARCH_EXP,
+  SERIES,
 } from "./_constants";
 
 const { apiEndpoint } = config.env.production;
@@ -24,6 +26,29 @@ const fetchUrls = async (exp) => {
     throw new Error(error.message);
   }
   return response.results.map((d) => d.url);
+};
+
+/**
+ * Fetches data from the events endpoint in Cal-Adapt API
+ * Identify ensemble min & max series, these will be used to calculate the envelope/range
+ * Input parameters:
+ * url - url of raster series in API
+ * params - object with props for geometry, stat, units, etc.
+ * method - default is GET, POST used for user uploaded boundaries
+ * @param {object}
+ * @return {array}
+ */
+const fetchEvents = async ({ url, params, method = "GET" }) => {
+  const [response, error] = await handleXHR(
+    fetchData(`${url}events/`, params, method)
+  );
+  if (error) {
+    throw new Error(error.message);
+  }
+  const values = transformResponse(response);
+  const { id } = SERIES.find(({ id }) => url.includes(id));
+  const isRange = url.search(ENVELOPE_SEARCH_EXP) > 0 ? true : false;
+  return { id, isRange, values };
 };
 
 /**
@@ -56,7 +81,9 @@ export async function getProjections(config, params, method = "GET") {
     const { indicatorId } = config;
     const exp = DEFAULT_PROJECTIONS_SLUG_EXP.replace("indicator", indicatorId);
     const urls = await fetchUrls(exp);
-    return urls;
+    const promises = urls.map((url) => fetchEvents({ url, params }));
+    const data = await Promise.all(promises);
+    return data;
   } catch (error) {
     throw new Error(error.message);
   }
