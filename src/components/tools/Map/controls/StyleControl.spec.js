2fe01fa2edbeafe2bd355eefbb15cf99015948a8
Chris Henrick <chrishenrick@gmail.com>
2022-02-15 11:54:55 -0800

    finish test suite for Map/controls/StyleControl component

--- src/components/tools/Map/controls/StyleControl.spec.js
+++ src/components/tools/Map/controls/StyleControl.spec.js
@@ -2,28 +2,55 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/svelte";
+import { render, fireEvent } from "@testing-library/svelte";
 import StyleControl from "./StyleControl.svelte";
 
 describe("Map/controls/StyleControl", () => {
-  const styles = [
-    { label: "Light", id: "light-v10" },
-    { label: "Dark", id: "dark-v10" },
-    { label: "Satellite", id: "satellite-v9" },
-    { label: "Satellite with Streets", id: "satellite-streets-v11" },
-  ];
+  let target;
+
+  beforeEach(() => {
+    target = document.body.appendChild(document.createElement("slot"));
+  });
 
   test("should render", async () => {
-    const target = document.body.appendChild(document.createElement("slot"));
+    const result = render(StyleControl);
+    expect(result).toBeTruthy();
+  });
+
+  test("titleText prop", async () => {
     const { getByText, component } = render(StyleControl, {
       target,
-      props: {
-        titleText: "Select Map Style",
-        styles,
-        selected: "dark-v10",
-      },
     });
     await component.$set({ titleText: "Select a Style" });
     expect(() => getByText("Select a Style")).not.toThrow();
+  });
+
+  test("styles prop", async () => {
+    const styles = [
+      { label: "Light", id: "light-v10" },
+      { label: "Dark", id: "dark-v10" },
+    ];
+    const { queryAllByRole, component } = render(StyleControl, { target });
+    await component.$set({ styles });
+    const result = queryAllByRole("radio");
+    expect(result).toHaveLength(2);
+  });
+
+  test("selected prop", async () => {
+    const { getByLabelText, component } = render(StyleControl, { target });
+    await component.$set({ selected: "dark-v10" });
+    const result = getByLabelText("Dark").checked;
+    expect(result).toBe(true);
+  });
+
+  test("change event", () => {
+    const { getByLabelText, component } = render(StyleControl, {
+      target,
+    });
+    const mock = jest.fn();
+    const radioBtn = getByLabelText("Satellite");
+    component.$on("change", (event) => mock(event.detail));
+    fireEvent.click(radioBtn);
+    expect(mock).toHaveBeenCalledWith("satellite-v9");
   });
 });
