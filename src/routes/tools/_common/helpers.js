7f7e580a073043371fef0f2cb5381c565af9bb24
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2022-02-23 16:53:15 -0800

    use extent to get min/max for array

--- src/routes/tools/_common/helpers.js
+++ src/routes/tools/_common/helpers.js
@@ -26,11 +26,11 @@ export function buildEnvelope(_data) {
     (d) => d.date.getUTCFullYear()
   );
   return dataArr.map(([key, value]) => {
-    const sortedArr = sort(value);
+    const arrExtent = extent(value);
     return {
       date: new Date(Date.UTC(key, 0, 1)),
-      min: sortedArr[0],
-      max: sortedArr[1],
+      min: arrExtent[0],
+      max: arrExtent[1],
     };
   });
 }
