f27d88485fe7d9c0de6e3c1dc136cd2ce0def2e5
Chris Henrick <chrishenrick@gmail.com>
2022-04-15 18:44:01 -0700

    sanitize search string before geocoding

--- src/routes/tools/local-climate-change-snapshot/_select-location/geocode-search.js
+++ src/routes/tools/local-climate-change-snapshot/_select-location/geocode-search.js
@@ -5,9 +5,18 @@ import {
 } from "~/helpers/geocode";
 import { serialize } from "~/helpers/utilities";
 
+/**
+ *
+ * @param {string} searchStr – string to geocode
+ * @param {Object} options - additional options
+ * @param {string} options.boundaryType - type of geography, e.g. address, counties, etc.
+ * @param {AbortSignal} options.signal - signal object from an AbortController instance
+ * @returns Object - result of Response.json()
+ */
 export async function geocodeSearch(searchStr, options = {}) {
   const { boundaryType, signal } = options;
   let url;
+  searchStr = sanitizeSearchStr(searchStr, boundaryType);
   if (boundaryType === "address") {
     url = `${mapboxGeocodingEndpoint}/${searchStr}.json?${serialize(
       mapboxGeocodeParams
@@ -26,4 +35,30 @@ export async function geocodeSearch(searchStr, options = {}) {
   });
   const result = await response.json();
   return result;
+}
+
+/**
+ * sanitizeSearch: removes extraneous characters that the cal-adapt geocoding API won't match.
+ * @param {string} searchStr – search string
+ * @param {string} boundaryType – geographic boundary type
+ * @returns string
+ */
+function sanitizeSearchStr(searchStr, boundaryType) {
+  let regex;
+  switch (boundaryType) {
+    case "counties":
+      regex = /\scounty/i;
+      break;
+    case "censustracts":
+      regex = /census\stract\s/i;
+      break;
+    case "hydrounits":
+      regex = /\swatershed/i;
+      break;
+    default:
+  }
+  if (regex) {
+    searchStr = searchStr.replace(regex, "");
+  }
+  return searchStr.replace(/,\scalifornia/i, "");
 }
