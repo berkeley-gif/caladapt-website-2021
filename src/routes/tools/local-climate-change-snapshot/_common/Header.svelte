6094b56039b3bf06d50450365941fd07ef7475ef
Chris Henrick <chrishenrick@gmail.com>
2022-04-22 16:18:20 -0700

    remove use of html header elment in Header component

--- src/routes/tools/local-climate-change-snapshot/_common/Header.svelte
+++ src/routes/tools/local-climate-change-snapshot/_common/Header.svelte
@@ -53,64 +53,62 @@
   }
 </style>
 
-<header>
-  <Grid>
-    <Row>
-      <Column padding="{true}">
-        <div class="banner--icons">
-          {#each iconPaths as path}
-            <img src="{path}" alt="" aria-hidden="true" class="icon" />
-          {/each}
+<Grid>
+  <Row>
+    <Column padding="{true}">
+      <div class="banner--icons">
+        {#each iconPaths as path}
+          <img src="{path}" alt="" aria-hidden="true" class="icon" />
+        {/each}
+      </div>
+    </Column>
+  </Row>
+  <Row>
+    <Column lg="{10}" md="{8}" sm="{4}">
+      <h1>{@html titleText}</h1>
+      {#if pageName === "explore"}
+        <div class="header-btn-container">
+          <Button on:click="{noop}" size="field" icon="{Pdf16}"
+            >Generate Report</Button
+          >
+          <Button
+            on:click="{changeLocation}"
+            size="field"
+            icon="{Location16}"
+            kind="tertiary">Change Location</Button
+          >
         </div>
-      </Column>
-    </Row>
+      {/if}
+    </Column>
+    <Column lg="{{ span: 4, offset: 2 }}" md="{8}" sm="{4}">
+      {#if pageName === "explore"}
+        <StaticMap height="{250}" location="{location}" useButton="{false}" />
+      {/if}
+    </Column>
+  </Row>
+  {#if pageName === "index"}
     <Row>
-      <Column lg="{10}" md="{8}" sm="{4}">
-        <h1>{@html titleText}</h1>
-        {#if pageName === "explore"}
-          <div class="header-btn-container">
-            <Button on:click="{noop}" size="field" icon="{Pdf16}"
-              >Generate Report</Button
-            >
-            <Button
-              on:click="{changeLocation}"
-              size="field"
-              icon="{Location16}"
-              kind="tertiary">Change Location</Button
-            >
-          </div>
-        {/if}
+      <Column lg="{8}" md="{8}" sm="{4}">
+        <p class="lead">
+          Climate change related effects vary significantly throughout
+          California, mirroring our state’s diverse climate, topography, and
+          ecology. This tool is a starting place if you are looking to get a
+          quick sense of climate impacts in your region. The Snapshot tool
+          provides climate projections for temperature, precipitation, and
+          wildfire. Additional variables e.g. sea level rise will be added when
+          they become available.
+        </p>
       </Column>
-      <Column lg="{{ span: 4, offset: 2 }}" md="{8}" sm="{4}">
-        {#if pageName === "explore"}
-          <StaticMap height="{250}" location="{location}" useButton="{false}" />
-        {/if}
+      <Column lg="{8}" md="{8}" sm="{4}">
+        <p class="lead">
+          The Local Climate Change Snapshot Tool tool is designed to be
+          straightforward and accessible for most users. Watch a <a
+            href="https://www.youtube.com/watch?v=qcXtv2LpWr0"
+            target="_blank">short video</a
+          > on how to use the tool. If you want to explore the data in more depth,
+          other tools on Cal-Adapt provide more configurable options.
+        </p>
       </Column>
     </Row>
-    {#if pageName === "index"}
-      <Row>
-        <Column lg="{8}" md="{8}" sm="{4}">
-          <p class="lead">
-            Climate change related effects vary significantly throughout
-            California, mirroring our state’s diverse climate, topography, and
-            ecology. This tool is a starting place if you are looking to get a
-            quick sense of climate impacts in your region. The Snapshot tool
-            provides climate projections for temperature, precipitation, and
-            wildfire. Additional variables e.g. sea level rise will be added
-            when they become available.
-          </p>
-        </Column>
-        <Column lg="{8}" md="{8}" sm="{4}">
-          <p class="lead">
-            The Local Climate Change Snapshot Tool tool is designed to be
-            straightforward and accessible for most users. Watch a <a
-              href="https://www.youtube.com/watch?v=qcXtv2LpWr0"
-              target="_blank">short video</a
-            > on how to use the tool. If you want to explore the data in more depth,
-            other tools on Cal-Adapt provide more configurable options.
-          </p>
-        </Column>
-      </Row>
-    {/if}
-  </Grid>
-</header>
+  {/if}
+</Grid>
