2ac6d60850ba2d732f201579c81ab0cec729713d
Chris Henrick <chrishenrick@gmail.com>
2022-05-04 15:19:57 -0700

    added fix me comment for StaticMap createOverlay fn

--- src/components/tools/Location/StaticMap.svelte
+++ src/components/tools/Location/StaticMap.svelte
@@ -66,6 +66,7 @@
     )}`;
   }
 
+  // FIXME: this creates invalid geometries for some boundaries, e.g. Tehama County
   function createOverlay(geojson, tolerance = 0.005) {
     const overlay = encodeURIComponent(JSON.stringify(geojson));
     if (overlay.length < 7500) {
