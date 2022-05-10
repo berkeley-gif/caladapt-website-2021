c283bf4c0fef72b46b208e697dcd8e44c44241c1
Chris Henrick <chrishenrick@gmail.com>
2022-05-10 10:33:36 -0700

    updated tests

--- src/routes/tools/_common/helpers.spec.js
+++ src/routes/tools/_common/helpers.spec.js
@@ -4,19 +4,26 @@ import { setInitialLocation } from "./helpers";
 import { DEFAULT_LOCATION } from "./constants";
 
 describe("tools/_common/helpers", () => {
-  test("setInitialLocation default location", () => {
-    return expect(setInitialLocation()).resolves.toEqual(DEFAULT_LOCATION);
+  test("setInitialLocation returns default location", () => {
+    const expected = {
+      location: DEFAULT_LOCATION,
+      boundaryType: "locagrid",
+    };
+    return expect(setInitialLocation()).resolves.toEqual(expected);
   });
 
   test("setInitialLocation using lng lat", () => {
     const [lng, lat] = [-122.09, 37.78];
     const expected = {
-      bbox: [0, 0, 0, 0],
-      center: [0, 0],
-      geometry: {},
-      properties: {},
-      id: 1234,
-      title: "LOCA Grid Cell",
+      location: {
+        bbox: [0, 0, 0, 0],
+        center: [0, 0],
+        geometry: {},
+        properties: {},
+        id: 1234,
+        title: "LOCA Grid Cell",
+      },
+      boundaryType: "locagrid",
     };
     return expect(setInitialLocation(lng, lat, "locagrid")).resolves.toEqual(
       expected
@@ -26,12 +33,15 @@ describe("tools/_common/helpers", () => {
   test("setInitialLocation using featureId and boundary", () => {
     const featureId = 1;
     const expected = {
-      bbox: [0, 0, 0, 0],
-      center: [0, 0],
-      geometry: {},
-      properties: {},
-      id: featureId,
-      title: "New place name",
+      location: {
+        bbox: [0, 0, 0, 0],
+        center: [0, 0],
+        geometry: {},
+        properties: {},
+        id: featureId,
+        title: "New place name",
+      },
+      boundaryType: "counties",
     };
     return expect(
       setInitialLocation(null, null, "counties", 1)
