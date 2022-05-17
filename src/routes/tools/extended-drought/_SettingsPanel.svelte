2656f8ba60bad85c191a3b9536fcfce98cb1bd16
Chris Henrick <chrishenrick@gmail.com>
2022-05-17 13:01:28 -0700

    Extended Drought uses LocationMap component

--- src/routes/tools/extended-drought/_SettingsPanel.svelte
+++ src/routes/tools/extended-drought/_SettingsPanel.svelte
@@ -10,7 +10,7 @@
     RadioBtnGroup,
   } from "~/components/tools/Settings";
   import { LearnMoreButton } from "~/components/tools/Partials";
-  import { StaticMap } from "~/components/tools/Location";
+  import { LocationMap } from "~/components/tools/Location";
 
   import { locationStore } from "../_common/stores";
   import {
@@ -48,11 +48,7 @@
 <!-- Chart Settings -->
 <div class="block">
   <span class="bx--label">Select Location</span>
-  <StaticMap
-    location="{$location}"
-    height="{250}"
-    on:click="{() => showChangeLocation()}"
-  />
+  <LocationMap location="{$location}" on:click="{() => showChangeLocation()}" />
   <div class="center-row">
     <LearnMoreButton
       on:click="{() =>
