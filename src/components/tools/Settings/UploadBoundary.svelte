ac004304b6c3f81ae2199606ac7bf3edb2db62dd
Chris Henrick <chrishenrick@gmail.com>
2022-05-23 16:54:50 -0700

    refactored UploadBoundary processUpload

--- src/components/tools/Settings/UploadBoundary.svelte
+++ src/components/tools/Settings/UploadBoundary.svelte
@@ -30,7 +30,7 @@
     fileUploader.clearFiles();
   }
 
-  function clearFiles() {
+  export function clearFiles() {
     fileUploader.clearFiles();
     fileUploadProps.status = "";
     fileUploadProps.errorSubject = "";
@@ -39,50 +39,101 @@
     dispatch("clear");
   }
 
-  async function processUpload(file) {
-    console.log("....uploadng");
+  function handleLoading() {
     fileUploadProps.status = "uploading";
     fileUploadProps.invalid = false;
     fileUploadProps.errorSubject = "";
     fileUploadProps.errorBody = "";
-    const [validity, validityError] = await handleXHR(validateShape(file));
-    if (validityError) {
-      fileUploadProps.status = "edit";
-      fileUploadProps.invalid = true;
-      fileUploadProps.errorSubject = "Error";
-      fileUploadProps.errorBody = String(validityError).split(":")[1];
-      return;
-    }
-    fileUploadProps.status = "complete";
-    const [geojson, geojsonError] = await handle(convertFileToGeojson(file));
+  }
 
-    if (geojson && !geojsonError && geojson.features.length === 1) {
-      fileUploadProps.errorSubject = "";
-      fileUploadProps.errorBody = "";
-      fileUploadProps.invalid = false;
-      const location = formatFeature(geojson.features[0], "custom");
-      location.title = file.name;
-      dispatch("upload", { location });
-      return;
-    }
+  function handleValidityError(error) {
+    fileUploadProps.status = "edit";
+    fileUploadProps.invalid = true;
+    fileUploadProps.errorSubject = "Error";
+    fileUploadProps.errorBody = String(error).split(":")[1];
+  }
 
-    if (geojson && geojsonError && geojson.features.length === 1) {
+  function handleGeoJsonError(data, error) {
+    if (data && data.features && data.features.length) {
       fileUploadProps.errorSubject = "Warning";
       fileUploadProps.errorBody =
         "We can fetch data for this area but cannot display it on the inset map.";
-      const location = formatFeature(geojson.features[0], "custom");
-      location.title = file.name;
-      dispatch("upload", { location });
-      return;
+      // FIXME: support this use case???
+      // const location = formatFeature(geojson.features[0], "custom");
+      // location.title = file.name;
+      // dispatch("upload", { location });
+    } else {
+      fileUploadProps.errorSubject = "Error";
+      fileUploadProps.errorBody =
+        "Something went wrong, please contact support.";
+      fileUploadProps.invalid = true;
     }
+  }
 
-    if (geojson && geojson.features.length > 1) {
+  function handleGeoJsonSuccess(data) {
+    if (data.type === "FeatureCollection") {
+      if (data.features.length) {
+        const flag = data.features.length > 1;
+        fileUploadProps.status = flag ? "edit" : "complete";
+        fileUploadProps.errorSubject = flag ? "Warning" : "";
+        fileUploadProps.errorBody = flag
+          ? "The uploaded file has more than 1 geometry. The tool can display fetch data for only 1 geometry at a time."
+          : "";
+        fileUploadProps.invalid = flag;
+        return data.features[0];
+      } else {
+        fileUploadProps.status = "edit";
+        fileUploadProps.invalid = true;
+        fileUploadProps.errorSubject = "Error";
+        fileUploadProps.errorBody = "Uploaded file contains no features.";
+        return null;
+      }
+    } else if (data.type === "Feature") {
+      fileUploadProps.errorSubject = "";
+      fileUploadProps.errorBody = "";
+      fileUploadProps.invalid = false;
+      return data;
+    } else {
       fileUploadProps.status = "edit";
       fileUploadProps.invalid = true;
       fileUploadProps.errorSubject = "Error";
       fileUploadProps.errorBody =
-        "The uploaded file has more than 1 geometry. The tool can display fetch data for only 1 geometry at a time.";
+        "Must be a GeoJSON FeatureCollection or Feature.";
+      return null;
+    }
+  }
+
+  async function processUpload(file) {
+    handleLoading();
+
+    // NOTE: this only validates the file against the Cal-Adapt API.
+    // The response is not used for anything.
+    const [, validityError] = await handleXHR(validateShape(file));
+
+    if (validityError) {
+      handleValidityError(validityError);
       return;
+    }
+
+    // Try converting the uploaded boundary to (Geo)JSON format
+    const [geojson, geojsonError] = await handle(convertFileToGeojson(file));
+
+    if (geojsonError) {
+      handleGeoJsonError(geojson, geojsonError);
+      return;
+    }
+
+    if (typeof geojson === "object" && "type" in geojson) {
+      const feature = handleGeoJsonSuccess(geojson);
+      if (feature) {
+        const location = formatFeature(feature, "custom");
+        location.title = file.name;
+        dispatch("upload", { location });
+      } else {
+        handleGeoJsonError(geojson);
+      }
+    } else {
+      handleGeoJsonError(geojson);
     }
   }
 </script>
