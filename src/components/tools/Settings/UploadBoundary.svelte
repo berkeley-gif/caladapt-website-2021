67782e52dda5851177adc217c23fe26eaa7761c2
Chris Henrick <chrishenrick@gmail.com>
2022-05-24 12:08:31 -0700

    more refactoring of UploadBoundary component

--- src/components/tools/Settings/UploadBoundary.svelte
+++ src/components/tools/Settings/UploadBoundary.svelte
@@ -8,20 +8,30 @@
     handle,
     convertFileToGeojson,
     validateShape,
-  } from "./../../../helpers/utilities";
-  import { formatFeature } from "./../../../helpers/geocode";
+  } from "~/helpers/utilities";
+  import { formatFeature } from "~/helpers/geocode";
 
   const dispatch = createEventDispatcher();
+
+  const labelDescription = `Select a boundary from the dropdown list or upload
+    your project area. Supported formats - zipped Shapefile, GeoJSON, KML.`;
+
+  const statuses = {
+    uploading: "uploading",
+    edit: "edit",
+    complete: "complete",
+  };
+
   const fileUploadProps = {
     labelTitle: "",
-    labelDescription:
-      "Select a boundary from the dropdown list or upload your project area. Supported formats - zipped shapefile, GeoJSON, KML, WKT.",
+    labelDescription,
     buttonLabel: "Upload File",
     accept: [".zip", ".json", ".geojson", ".kml"],
     iconDescription: "Clear file",
-    id: "Upload",
-    errorSubject: "",
-    errorBody: "",
+    id: "upload-custom-boundary",
+    errorSubject: "", // not actually a FileUploader prop
+    errorBody: "", // not actually a FileUploader prop
+    status: statuses.uploading,
   };
 
   let fileUploader;
@@ -32,7 +42,7 @@
 
   export function clearFiles() {
     fileUploader.clearFiles();
-    fileUploadProps.status = "";
+    fileUploadProps.status = statuses.uploading;
     fileUploadProps.errorSubject = "";
     fileUploadProps.errorBody = "";
     fileUploadProps.invalid = false;
@@ -40,65 +50,46 @@
   }
 
   function handleLoading() {
-    fileUploadProps.status = "uploading";
+    fileUploadProps.status = statuses.uploading;
     fileUploadProps.invalid = false;
     fileUploadProps.errorSubject = "";
     fileUploadProps.errorBody = "";
   }
 
-  function handleValidityError(error) {
-    fileUploadProps.status = "edit";
-    fileUploadProps.invalid = true;
-    fileUploadProps.errorSubject = "Error";
-    fileUploadProps.errorBody = String(error).split(":")[1];
+  /** @type {(error: Error | string, title: "Error" | "Warning") => void} */
+  function handleValidityError(error, title = "Error") {
+    fileUploadProps.status = statuses.edit;
+    fileUploadProps.invalid = title === "Error";
+    fileUploadProps.errorSubject = title;
+    fileUploadProps.errorBody =
+      typeof error === "object" ? error.message : error;
   }
 
-  function handleGeoJsonError(data, error) {
-    if (data && data.features && data.features.length) {
-      fileUploadProps.errorSubject = "Warning";
-      fileUploadProps.errorBody =
-        "We can fetch data for this area but cannot display it on the inset map.";
-      // FIXME: support this use case???
-      // const location = formatFeature(geojson.features[0], "custom");
-      // location.title = file.name;
-      // dispatch("upload", { location });
-    } else {
-      fileUploadProps.errorSubject = "Error";
-      fileUploadProps.errorBody =
-        "Something went wrong, please contact support.";
-      fileUploadProps.invalid = true;
-    }
-  }
-
+  /** @type {(data: GeoJSON) => Feature | null } */
   function handleGeoJsonSuccess(data) {
     if (data.type === "FeatureCollection") {
       if (data.features.length) {
         const flag = data.features.length > 1;
-        fileUploadProps.status = flag ? "edit" : "complete";
+        fileUploadProps.status = statuses.complete;
         fileUploadProps.errorSubject = flag ? "Warning" : "";
         fileUploadProps.errorBody = flag
-          ? "The uploaded file has more than 1 geometry. The tool can display fetch data for only 1 geometry at a time."
+          ? `The uploaded file has more than 1 geometry. The tool can display
+            data for only 1 geometry at a time.`
           : "";
-        fileUploadProps.invalid = flag;
+        fileUploadProps.invalid = false;
         return data.features[0];
       } else {
-        fileUploadProps.status = "edit";
-        fileUploadProps.invalid = true;
-        fileUploadProps.errorSubject = "Error";
-        fileUploadProps.errorBody = "Uploaded file contains no features.";
+        handleValidityError("The uploaded file contains no features.");
         return null;
       }
     } else if (data.type === "Feature") {
+      fileUploadProps.status = statuses.complete;
       fileUploadProps.errorSubject = "";
       fileUploadProps.errorBody = "";
       fileUploadProps.invalid = false;
       return data;
     } else {
-      fileUploadProps.status = "edit";
-      fileUploadProps.invalid = true;
-      fileUploadProps.errorSubject = "Error";
-      fileUploadProps.errorBody =
-        "Must be a GeoJSON FeatureCollection or Feature.";
+      handleValidityError("Must be a GeoJSON FeatureCollection or Feature.");
       return null;
     }
   }
@@ -115,47 +106,57 @@
       return;
     }
 
-    // Try converting the uploaded boundary to (Geo)JSON format
+    // Try converting the uploaded boundary to JSON format
     const [geojson, geojsonError] = await handle(convertFileToGeojson(file));
+    const isValidGeoJson = typeof geojson === "object" && "type" in geojson;
 
     if (geojsonError) {
-      handleGeoJsonError(geojson, geojsonError);
-      return;
+      if (isValidGeoJson) {
+        handleValidityError(
+          `We can fetch data for this area, but the boundary may not display on
+          the location map.`,
+          "Warning"
+        );
+        // NOTE: we don't early return here so that the custom boundary may still be used
+      } else {
+        handleValidityError(geojsonError);
+        return;
+      }
     }
 
-    if (typeof geojson === "object" && "type" in geojson) {
+    if (isValidGeoJson) {
       const feature = handleGeoJsonSuccess(geojson);
       if (feature) {
         const location = formatFeature(feature, "custom");
         location.title = file.name;
         dispatch("upload", { location });
-      } else {
-        handleGeoJsonError(geojson);
       }
-    } else {
-      handleGeoJsonError(geojson);
     }
   }
 </script>
 
 <style>
-  :global(.bx--file__selected-file) {
+  div :global(.bx--file__selected-file) {
     background: #f1f4f8;
   }
-  :global(.bx--file__selected-file .bx--file-filename) {
+
+  div :global(.bx--file__selected-file .bx--file-filename) {
     margin-bottom: 0;
   }
 </style>
 
-<FileUploader
-  {...fileUploadProps}
-  kind="tertiary"
-  bind:this="{fileUploader}"
-  on:add="{({ detail }) => {
-    processUpload(detail[0]);
-  }}"
-  on:click="{clearFiles}"
-/>
-{#if fileUploadProps.errorSubject === "Warning" || fileUploadProps.errorSubject === "Error"}
-  <span>{fileUploadProps.errorBody}</span>
-{/if}
+<div>
+  <FileUploader
+    {...fileUploadProps}
+    kind="tertiary"
+    bind:this="{fileUploader}"
+    on:add="{({ detail }) => {
+      processUpload(detail[0]);
+    }}"
+    on:click="{clearFiles}"
+  />
+
+  {#if fileUploadProps.errorSubject}
+    <span>{fileUploadProps.errorSubject}: {fileUploadProps.errorBody}</span>
+  {/if}
+</div>
