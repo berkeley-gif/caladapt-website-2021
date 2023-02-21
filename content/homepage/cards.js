a21e30c99c3e47c28afc3d413df05af149f03fa6
melijimenez11 <melijimenez@berkeley.edu>
2023-02-20 21:56:09 -0800

    added 4th assessment block

--- content/homepage/cards.js
+++ content/homepage/cards.js
@@ -1,12 +1,5 @@
 export default [
   {
-    titleText: "New to Cal-Adapt?",
-    linkPath: "/help/get-started",
-    description:
-      "Learn how to get started with using climate data for California.",
-    bgColor: "var(--card-gradient-01)",
-  },
-  {
     titleText: "Local Climate Change Snapshot Tool",
     linkPath: "/tools/local-climate-change-snapshot",
     description:
@@ -24,21 +17,7 @@ export default [
     titleText: "Download Data",
     linkPath: "/data",
     description:
-      "Download climate data in NetCDF, CSV and GeoTIFF formats for your area.",
+      "Download 4th Assessment climate data in NetCDF, GeoTIFF and CSV formats.",
     bgColor: "var(--card-gradient-03)",
-  },
-  {
-    titleText: "Tutorials & Webinars",
-    linkPath: "/help/tutorials",
-    description:
-      "Browse our video collection of tool tutorials and past webinars.",
-    bgColor: "var(--card-gradient-02)",
-  },
-  {
-    titleText: "Developers",
-    linkPath: "/data/#cal-adapt-api",
-    description:
-      "Integrate climate data in your workflows with the Cal-Adapt API.",
-    bgColor: "var(--card-gradient-01)",
   },
 ];
--- src/routes/homepage-redesign-preview/index.svelte
+++ src/routes/homepage-redesign-preview/index.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import { Grid, Row, Column } from "carbon-components-svelte";
   import { Banner, SidebarRight } from "~/partials";
   import { Card, CardsContainer } from "~/components/cards";
@@ -102,5 +102,161 @@
     </Column>
   </Row>
 </Grid>
+
+<div class="spacing--v-96"></div>
+ -->
+<script context="module">
+  export async function preload() {
+    const posts = await this.fetch(`blog.json`)
+      .then((r) => r.json())
+      .then((posts) => posts);
+    const events = await this.fetch(`events.json`)
+      .then((r) => r.json())
+      .then((events) => events);
+    const data = await this.fetch("index.json")
+      .then((r) => r.json())
+      .then((data) => data);
+    return {
+      posts,
+      events,
+      cardsData: data.cardsData,
+      revealNewHomepage: data.revealNewHomepage,
+    };
+  }
+</script>
+
+<script>
+  import { Button } from "carbon-components-svelte";
+  import { ArrowRight16 } from "carbon-icons-svelte";
+  import { Card, CardsContainer } from "~/components/cards";
+  import { Banner, SidebarRight } from "~/partials";
+
+  export let events;
+  export let posts;
+  export let cardsData;
+  export let revealNewHomepage;
+
+  const icons = [
+    "sun",
+    "rainfall",
+    "wildfire",
+    "snowflake",
+    "sea",
+    "streamflow",
+  ].map((name) => `img/icons/${name}.svg`);
+  const cardHeight = 18;
+  const cardWidth = 18;
+</script>
+
+<style lang="scss">
+  .btn-container {
+    margin-top: 2.5rem;
+    text-transform: uppercase;
+  }
+</style>
+
+<svelte:head>
+  <title>Cal-Adapt</title>
+</svelte:head>
+
+<!-- Banner -->
+<Banner
+  titleText="Cal-Adapt provides a way to explore peer-reviewed data that 
+    portrays how climate change might affect California at the state and local
+    levels."
+  subtitleText="We make this data available through downloads, visualizations, 
+    and the Cal-Adapt API for your research, outreach, and adaptation planning
+    needs."
+  bannerImg="/img/banners/yosemite_1600x540.jpg"
+  bannerImgMobile="/img/banners/yosemite_700x800.jpg"
+  overlayOpacity="{0.4}"
+  titleFontSize="2rem"
+  titleFontWeight="600"
+  iconPaths="{icons}"
+  useOffset="{false}"
+>
+  <div class="btn-container" slot="button">
+    <Button icon="{ArrowRight16}" href="/about" sapper:prefetch
+      >More about Cal-Adapt</Button
+    >
+  </div>
+</Banner>
+
+<div class="spacing--v-32"></div>
+
+<div class="bx--grid">
+  <div class="bx--row">
+    <div class="bx--col-lg-12 bx--col-md-8 bx--col-sm-4">
+      <div class="spacing--v-24"></div>
+      <h2>Explore interactive maps and charts</h2>
+      <div class="spacing--v-24"></div>
+      <p class="homepage">
+        Visualize and download <strong>downscaled CMIP5 climate data</strong>
+        and other datasets developed for California’s
+        <a href="#" target="_blank">4th Climate Change Assessment.</a>
+        Read our <a href="/get-started" target="_blank">Get Started</a> guide to
+        learn more about working with climate data.
+      </p>
+      <p class="homepage">
+        Designed for a <strong>broad range of users.</strong>
+      </p>
+      <div class="spacing--v-24"></div>
+      <CardsContainer gridGap="{2}" cardWidth="{cardWidth}">
+        {#each cardsData as cardDatum, index}
+          <Card
+            {...{
+              ...cardDatum,
+              height: cardHeight,
+              ctaText: "Learn more",
+              textColor: "white",
+              useRule: true,
+            }}
+          />
+        {/each}
+      </CardsContainer>
+
+      <div class="spacing--v-24"></div>
+      <h2>Analyze next generation climate data</h2>
+    </div>
+
+    <!-- Sidebar on desktop only -->
+    <div class="bx--col-lg-4 bx--col-md-0 bx--col-sm-0">
+      <SidebarRight
+        display="{['events', 'posts']}"
+        events="{events}"
+        posts="{[posts[0]]}"
+      />
+    </div>
+  </div>
+
+  <div class="bx--row">
+    <div class="bx--col">
+      <div class="spacing--v-48"></div>
+      <p class="lead">
+        Get the latest Cal-Adapt news, updates &amp; events delivered to your
+        inbox. Subscribe to the Cal-Adapt Newsletter.
+      </p>
+      <div class="btn-container">
+        <Button icon="{ArrowRight16}" href="/signup/#cal-adapt-newsletter"
+          >Subscribe</Button
+        >
+      </div>
+    </div>
+  </div>
+
+  <!-- TODO: remove this before deploying to production -->
+  {#if revealNewHomepage}
+    <div class="bx--row">
+      <div class="bx--col">
+        <div class="spacing--v-48"></div>
+        <p>
+          View the <a rel="external" href="/homepage-redesign-preview/"
+            >new homepage design.</a
+          >
+        </p>
+      </div>
+    </div>
+  {/if}
+</div>
 
 <div class="spacing--v-96"></div>
