f1a3bb61f8fa29a63fbbaa83e9f546d02d00cd10
melijimenez11 <124095974+melijimenez11@users.noreply.github.com>
2023-03-21 13:06:47 -0700

    Implement grants page (#260)
    * added grants notice to the landing page
    
    * added grants page
    
    * some comments and organizing
    
    * made a content file for the grants block
    
    * fixed grants block on the homepage
    
    * resolved svelte scrolling bug
    
    * updated content for grants and research
    
    * added h2 headers for accessibility
    
    * added content load to the preload module
    
    * fix link in memorandum WRF
    
    * added space between tables

---
+++ content/grants/research.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    agency: "California Energy Commission",
+    type: `Research Project Description`,
+    number: "EPC-20-006",
+    title: "Development of Climate Projections for California and Identification of Priority Projections",
+    url: "https://www.energizeinnovation.fund/projects/development-climate-projections-california-and-identification-priority-projections"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Memo`,
+    number: "EPC-20-006",
+    title: "2021 - Nov: Memorandum on Evaluating Global Climate Models for Studying Regional Climate Change in California",
+    url: "https://www.energy.ca.gov/media/7264"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Memo`,
+    number: "EPC-20-006",
+    title: "2022 - May: Memorandum on the Development and Availability of Dynamically Downscaled Projections Using WRF",
+    url: "https://www.energy.ca.gov/media/7265"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Memo`,
+    number: "EPC-20-006",
+    title: "2022 - Nov: Memorandum on the Evaluation of Dynamically Downscaled GCMs Using WRF",
+    url: "https://www.energy.ca.gov/sites/default/files/2022-09/20220907_CDAWG_MemoDynamicalDownscaling_EPC-20-006_May2022-ADA.pdfs"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Project Description`,
+    number: "PIR-19-006",
+    title: "Climate Analytics to Support Natural Gas Sector Utilities: Actionable, Responsive and Open Solutions for Historical Climate Needs in California",
+    url: "https://www.energizeinnovation.fund/projects/climate-analytics-support-natural-gas-sector-utilities-actionable-responsive-and-open"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Project Description`,
+    number: "PIR-19-007",
+    title: "Development and Evaluation of a High-Resolution Historical Climate Dataset over California",
+    url: "https://www.energizeinnovation.fund/projects/development-and-evaluation-high-resolution-historical-climate-dataset-over-california"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Project Description`,
+    number: "EPC-18-026",
+    title: "Comprehensive Open Source Development of Next Generation Wildfire Models for Grid Resiliency",
+    url: "https://www.energizeinnovation.fund/projects/comprehensive-open-source-development-next-generation-wildfire-models-grid-resiliency"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Research Website`,
+    number: "EPC-18-026",
+    title: "Pyregence, Open Source Fire Science",
+    url: "https://pyregence.org/"
+  },
+];
