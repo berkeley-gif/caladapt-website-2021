b1a558171808fd5d97b0c400b2cfcd803343fbd5
melijimenez11 <124095974+melijimenez11@users.noreply.github.com>
2023-03-21 13:06:47 -0700

    Implement grants page (#260)
    * added grants notice to the landing page
    
    * added grants page
    
    * some comments and organizing
    
    * made a content file for the grants block
    
    * fixed grants block on the homepage
    
    * resolved svelte scrolling bug
    
    * updated content for grants and research
    
    * added h2 headers for accessibility
    
    * added content load to the preload module
    
    * fix link in memorandum WRF
    
    * added space between tables

---
+++ content/grants/memos.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    agency: "California Energy Commission",
+    type: `Staff Memo`,
+    number: "TN239123",
+    title: "2021-Aug: Follow-up on Staff Workshop Regarding Research to Support a Climate-Resilient Transition to a Clean Electricity System",
+    url: "https://efiling.energy.ca.gov/GetDocument.aspx?tn=239123&DocumentContentId=72578"
+  },
+  {
+    agency: "California Energy Commission",
+    type: `Staff Memo`,
+    number: "TN23994",
+    title: "2021-Oct: An Expanded Cal-Adapt Enterprise – Roles, Responsibilities, and Authorities",
+    url: "https://efiling.energy.ca.gov/GetDocument.aspx?tn=239954&DocumentContentId=73401"
+  },
+];
