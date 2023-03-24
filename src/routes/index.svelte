05b43f2eb4e6098a319ee8a5b885d40381cdece8
Shruti Mukhtyar <mukhtyar@berkeley.edu>
2023-03-24 10:00:32 -0700

    remove temp cards variable

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,4 @@
 <script context="module">
-  // This is a temporary variable. We can get rid of it once the landing page changes have been finalized and approved
-  const cardsDataTemp = [
-    {
-      titleText: "Local Climate Change Snapshot Tool",
-      linkPath: "/tools/local-climate-change-snapshot",
-      description:
-        "Quickly view a variety of climate data for a city, county, or other place.",
-      bgColor: "var(--card-gradient-02)",
-    },
-    {
-      titleText: "Explore all Climate Tools",
-      linkPath: "/tools",
-      description:
-        "Explore data on temperature, precipitation, snowpack, wildfire, and more.",
-      bgColor: "var(--card-gradient-03)",
-    },
-    {
-      titleText: "Download Data",
-      linkPath: "/data",
-      description:
-        "Download Fourth Assessment climate data in NetCDF, GeoTIFF and CSV formats.",
-      bgColor: "var(--card-gradient-03)",
-    },
-  ];
-
   export async function preload() {
     const posts = await this.fetch(`blog.json`)
       .then((r) => r.json())
@@ -37,8 +12,7 @@
     return {
       posts,
       events,
-      //cardsData: data.cardsData,
-      cardsData: cardsDataTemp,
+      cardsData: data.cardsData,
     };
   }
 </script>
