b1ed3934c49eb4d6adfeba0cf05c38d1b5274cef
melijimenez11 <melijimenez@berkeley.edu>
2023-03-28 01:08:12 +0200

    changed order of research projects and other minor changes in grants page

--- content/grants/memos.js
+++ content/grants/memos.js
@@ -3,14 +3,14 @@ export default [
     agency: "California Energy Commission",
     type: `Staff Memo`,
     number: "TN239123",
-    title: "2021-Aug: Follow-up on Staff Workshop Regarding Research to Support a Climate-Resilient Transition to a Clean Electricity System",
+    title: "2021 - Aug: Follow-up on Staff Workshop Regarding Research to Support a Climate-Resilient Transition to a Clean Electricity System",
     url: "https://efiling.energy.ca.gov/GetDocument.aspx?tn=239123&DocumentContentId=72578"
   },
   {
     agency: "California Energy Commission",
     type: `Staff Memo`,
     number: "TN23994",
-    title: "2021-Oct: An Expanded Cal-Adapt Enterprise – Roles, Responsibilities, and Authorities",
+    title: "2021 - Oct: An Expanded Cal-Adapt Enterprise – Roles, Responsibilities, and Authorities",
     url: "https://efiling.energy.ca.gov/GetDocument.aspx?tn=239954&DocumentContentId=73401"
   },
 ];
